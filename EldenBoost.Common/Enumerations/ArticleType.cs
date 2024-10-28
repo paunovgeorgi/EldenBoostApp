e2e3de7ffb214cf4a95c157545dbeb04ad58f15c
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 20:17:21 +0200

    ArticleType enum created added to Enumerations.

---
+++ EldenBoost.Common/Enumerations/ArticleType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Common.Enumerations
+{
+    public enum ArticleType
+    {
+        News = 0,
+        Guide = 1
+    }
+}
