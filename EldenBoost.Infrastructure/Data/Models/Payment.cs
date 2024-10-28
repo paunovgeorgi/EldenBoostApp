cf874b95eecf08533cd874df8c2b19f346c758ea
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 16:51:16 +0200

    Platform entity created.

---
+++ EldenBoost.Infrastructure/Data/Models/Payment.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class Payment
+    {
+        [Key]
+        [Comment("Unique identifier for the Payment.")]
+        public int Id { get; set; }
+
+        [Comment("Total amount of the Payment.")]
+        public decimal Amount { get; set; }
+
+        [Comment("Indicates if the Payment has been completed.")]
+        public bool IsPaid { get; set; }
+
+        [Comment("Date when the Payment was issued.")]
+        public DateTime IssueDate { get; set; }
+
+        [Comment("Date when the Payment was completed.")]
+        public DateTime CompletionDate { get; set; }
+
+        [ForeignKey(nameof(Booster))]
+        [Comment("Foreign key linking the Payment to the associated Booster.")]
+        public int BoosterId { get; set; }
+
+        [Comment("Booster associated with the Payment.")]
+        public Booster Booster { get; set; } = null!;
+
+        [Comment("List of Orders linked to this Payment.")]
+        public List<Order> Orders { get; set; } = new List<Order>();
+    }
+}
