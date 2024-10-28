656baa009b287428e185e06d6f476d326fb821b4
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 17:20:01 +0200

    ApplicationType enum added for Application entity.

---
+++ EldenBoost.Common/Enumerations/ApplicationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Common.Enumerations
+{
+    public enum ApplicationType
+    {
+        Author = 0,
+        Booster = 1
+    }
+}
