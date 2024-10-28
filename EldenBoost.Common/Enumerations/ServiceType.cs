a0f85d5d2c5284077c7093b9d5c43e62f3986ebe
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 16:06:55 +0200

    Enumerations folder created in Common layer. ServiceType enum added.

---
+++ EldenBoost.Common/Enumerations/ServiceType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Common.Enumerations
+{
+    public enum ServiceType
+    {
+        Basic = 0,
+        Slider = 1,
+        Option = 2
+    }
+}
