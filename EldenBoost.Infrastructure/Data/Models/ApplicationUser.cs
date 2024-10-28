7fc6aed703b334ac1a4b344d0bf4e8edc7223f4b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 15:15:16 +0200

    ApplicationUser Entity created.

---
+++ EldenBoost.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static EldenBoost.Common.Constants.ValidationConstants.ApplicationUserValidations;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Comment("Nickname for application user")]
+        [Required]
+        [MaxLength(NicknameMaxLength)]
+        public string Nickname { get; set; } = null!;
+
+        [Comment("Profile picture application user.")]
+        [MaxLength(ProfilePictureMaxLength)]
+        public string? ProfilePicture { get; set; }
+
+    }
+}
