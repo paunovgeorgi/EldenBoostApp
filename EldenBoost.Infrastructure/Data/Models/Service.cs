aa8477d5c61a6f5e6d1db1cc3ad0dae12fb7a58a
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 16:07:29 +0200

    Service entity created.

---
+++ EldenBoost.Infrastructure/Data/Models/Service.cs
@@ -0,0 +1,48 @@
+﻿using EldenBoost.Common.Enumerations;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static EldenBoost.Common.Constants.ValidationConstants.ServiceValidations;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class Service
+    {
+        [Key]
+        [Comment("Service identifier.")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        [Comment("Service title.")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        [Comment("Service description.")]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Comment("Service price.")]
+        public decimal Price { get; set; }
+
+        [MaxLength(ImageUrlMaxLength)]
+        [Comment("Service image url.")]
+        public string? ImageURL { get; set; }
+
+        [Required]
+        [Comment("Flag for active and inactive services.")]
+        public bool IsActive { get; set; } = true;
+
+        [Comment("Service purchase count.")]
+        public int PurchaseCount { get; set; }
+
+        public int MaxAmount { get; set; }
+
+        [Comment("Service type")]
+        public ServiceType ServiceType { get; set; }
+
+        public ICollection<ServiceOption> Options { get; set; } = new List<ServiceOption>();
+    }
+}
