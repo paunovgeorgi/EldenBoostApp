5118d39a14900b5de3208bfc30a09b1f2e9aade7
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 16:56:48 +0200

    Many to many relationship for Booster and Platform initiated.

--- EldenBoost.Infrastructure/Data/EldenBoostDbContext.cs
+++ EldenBoost.Infrastructure/Data/EldenBoostDbContext.cs
@@ -10,5 +10,36 @@ namespace EldenBoost.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Booster>()
+                .HasMany(b => b.Platforms)
+                .WithMany(p => p.Boosters)
+                .UsingEntity<Dictionary<string, object>>(
+                "BoosterPlatform",
+                r => r
+                .HasOne<Platform>()
+                .WithMany()
+                .HasForeignKey("PlatformId")
+                .HasConstraintName("FK_BoosterPlatform_Platforms_Id")
+                .OnDelete(DeleteBehavior.Cascade),
+                r => r
+                .HasOne<Booster>()
+                .WithMany()
+                .HasForeignKey("BoosterId")
+                .HasConstraintName("FK_BoosterPlatform_Boosters_Id")
+                .OnDelete(DeleteBehavior.Cascade),
+                j =>
+                {
+                    j.HasKey("BoosterId", "PlatformId");
+                    j.ToTable("BoostersPlatforms");
+                    j.IndexerProperty<int>("BoosterId").HasColumnName("BoosterId");
+                    j.IndexerProperty<int>("PlatformId").HasColumnName("PlatformId");
+                });
+
+
+            base.OnModelCreating(builder);
+        }
     }
 }
