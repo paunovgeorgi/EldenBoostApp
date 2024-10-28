64ab4e6a86df74b870e9df93c967f9c2a0c91ba7
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 16:07:19 +0200

    Validation constants for Service entity added.

--- EldenBoost.Common/Constants/ValidationConstants.cs
+++ EldenBoost.Common/Constants/ValidationConstants.cs
@@ -9,5 +9,16 @@
 
             public const int ProfilePictureMaxLength = 2048;
         }
+
+        public static class ServiceValidations
+        {
+            public const int TitleMinLength = 3;
+            public const int TitleMaxLength = 80;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 248;
+
+            public const int ImageUrlMaxLength = 2048;
+        }
     }
 }
