1de97924ca9f6560651e3c6b6c36afe95523664c
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 15:14:58 +0200

    ValidationConstants static class created in Common layer.

---
+++ EldenBoost.Common/Constants/ValidationConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace EldenBoost.Common.Constants
+{
+    public static class ValidationConstants
+    {
+        public static class ApplicationUserValidations
+        {
+            public const int NicknameMinLength = 3;
+            public const int NicknameMaxLength = 64;
+
+            public const int ProfilePictureMaxLength = 2048;
+        }
+    }
+}
