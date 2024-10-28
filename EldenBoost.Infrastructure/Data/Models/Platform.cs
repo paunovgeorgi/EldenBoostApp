638b62e2bbbc48123e66af72b92142190ad878ea
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-28 16:57:44 +0200

    Platform entity created.

---
+++ EldenBoost.Infrastructure/Data/Models/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static EldenBoost.Common.Constants.ValidationConstants.PlatformValidations;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Comment("Unique identifier for the Platform.")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Comment("Name of the Platform.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Comment("Collection of Boosters associated with this Platform.")]
+        public ICollection<Booster> Boosters { get; set; } = new List<Booster>();
+
+        [Comment("Collection of Applications submitted for this Platform.")]
+        public ICollection<Application> Applications { get; set; } = new List<Application>();
+    }
+}
