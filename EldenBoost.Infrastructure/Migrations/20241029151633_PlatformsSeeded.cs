c0e8645e9aa15ffa9c3b7b583d2d3a2a33932c5f
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-29 17:18:20 +0200

    Platforms seeded to database.

---
+++ EldenBoost.Infrastructure/Migrations/20241029151633_PlatformsSeeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EldenBoost.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlatformsSeeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Platforms",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "PC" },
+                    { 2, "Playstation" },
+                    { 3, "Xbox" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Platforms",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
