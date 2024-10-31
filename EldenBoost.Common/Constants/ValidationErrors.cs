0f41db0d3aef8438986a81736944d9b246f95174
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-31 17:34:14 +0200

    ValidationErrors added to Common layer.

---
+++ EldenBoost.Common/Constants/ValidationErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace EldenBoost.Common.Constants
+{
+    public static class ValidationErrors
+    {
+        public const string FieldLength = "The {0} field must be between {2} and {1} characters.";
+    }
+}
