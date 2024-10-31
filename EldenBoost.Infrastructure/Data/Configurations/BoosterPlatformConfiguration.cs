2381c493d18a2e788e5e86d5cc8e258cb6e56ba4
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-31 16:56:31 +0200

    BoosterPlatformConfiguration created.

---
+++ EldenBoost.Infrastructure/Data/Configurations/BoosterPlatformConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using EldenBoost.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Infrastructure.Data.Configurations
+{
+    public class BoosterPlatformConfiguration : IEntityTypeConfiguration<BoosterPlatform>
+    {
+        public void Configure(EntityTypeBuilder<BoosterPlatform> builder)
+        {
+            builder.HasData(
+                new BoosterPlatform { BoosterId = 1, PlatformId = 1 }, // Cloud - PC
+                new BoosterPlatform { BoosterId = 1, PlatformId = 2 }, // Cloud - Playstation
+                new BoosterPlatform { BoosterId = 2, PlatformId = 1 }, // Mr. White - PC
+                new BoosterPlatform { BoosterId = 2, PlatformId = 2 }, // Mr. White - Playstation
+                new BoosterPlatform { BoosterId = 2, PlatformId = 3 }, // Mr. White - Xbox
+                new BoosterPlatform { BoosterId = 3, PlatformId = 1 }, // D.VA - PC
+                new BoosterPlatform { BoosterId = 3, PlatformId = 3 }, // D.VA - Xbox
+                new BoosterPlatform { BoosterId = 4, PlatformId = 2 }  // Mr. Wick - Playstation
+            );
+        }
+    }
+}
