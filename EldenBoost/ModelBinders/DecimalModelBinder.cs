9314526a613945bd3f4d9a242ff82517980abc18
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-31 22:42:12 +0200

    Decimal ModelBinder implemented.

---
+++ EldenBoost/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EldenBoost.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            ValueProviderResult valueResult =
+                bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrWhiteSpace(valueResult.FirstValue))
+            {
+                decimal parcedValue = 0m;
+                bool binderSucceeded = false;
+                try
+                {
+                    string formDecValue = valueResult.FirstValue.Trim();
+                    formDecValue = formDecValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    formDecValue = formDecValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    parcedValue = Convert.ToDecimal(formDecValue);
+                    binderSucceeded = true;
+                }
+                catch (Exception fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (binderSucceeded)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(parcedValue);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
