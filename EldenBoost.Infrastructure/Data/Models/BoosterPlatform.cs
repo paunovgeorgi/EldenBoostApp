b9b92684b4d1a573897b78d10727faa2339956da
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-10-31 16:55:59 +0200

    BoosterPlatform entity created in order to seed Booster data to the databae.

---
+++ EldenBoost.Infrastructure/Data/Models/BoosterPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class BoosterPlatform
+    {
+        public int BoosterId { get; set; }
+        public Booster Booster { get; set; } = null!;
+
+        public int PlatformId { get; set; }
+        public Platform Platform { get; set; } = null!;
+    }
+}
