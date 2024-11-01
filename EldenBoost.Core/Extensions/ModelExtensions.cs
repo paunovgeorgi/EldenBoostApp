4d1eafcabc7daeefa7f663bc83a89734167def4b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 15:36:05 +0200

    IServiceModel and IServiceModel model extenstion created.

---
+++ EldenBoost.Core/Extensions/ModelExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EldenBoost.Core.Models.Service.Contracts;
+using System.Text.RegularExpressions;
+
+namespace EldenBoost.Core.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static string GetInformation(this IServiceModel service)
+        {
+            string info = Regex.Replace(service.Title, @"\s+", "-");
+
+            info = Regex.Replace(info, @"[^a-zA-Z0-9\-]", string.Empty);
+
+            info = info.ToLower() + "-boost";
+
+            return info;
+        }
+    }
+}
---
+++ EldenBoost.Core/Models/Service/Contracts/IServiceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Models.Service.Contracts
+{
+    public interface IServiceModel
+    {
+        public string Title { get; set; }
+    }
+}
