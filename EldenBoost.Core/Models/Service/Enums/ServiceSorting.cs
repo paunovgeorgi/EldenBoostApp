a2edba5daa16f01cc27346f9f71a41889149fe6a
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 16:06:31 +0200

    ServiceSorting enum added.

---
+++ EldenBoost.Core/Models/Service/Enums/ServiceSorting.cs
@@ -0,0 +1,10 @@
+﻿namespace EldenBoost.Core.Models.Service.Enums
+{
+    public enum ServiceSorting
+    {
+        Newest = 0,
+        Oldest = 1,
+        PriceAscending = 2,
+        PriceDescending = 3,
+    }
+}
