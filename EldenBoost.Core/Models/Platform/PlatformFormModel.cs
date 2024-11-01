c23669768f194d2d17b920cd1d69c3666b709853
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 16:41:53 +0200

    PlatformFormModel created.

---
+++ EldenBoost.Core/Models/Platform/PlatformFormModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EldenBoost.Core.Models.Platform
+{
+    public class PlatformFormModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public bool IsChecked { get; set; }
+    }
+}
