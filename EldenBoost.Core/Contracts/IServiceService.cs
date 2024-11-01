74fd929e209d204c89c3d8412fa48eb22bf8c641
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 16:26:09 +0200

    AllAsync service task implemented.

--- EldenBoost.Core/Contracts/IServiceService.cs
+++ EldenBoost.Core/Contracts/IServiceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EldenBoost.Core.Models.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace EldenBoost.Core.Contracts
 {
     public interface IServiceService
     {
+        Task<AllServicesFilteredAndPagedModel> AllAsync(AllServicesQueryModel model);
     }
 }
--- EldenBoost.Core/Services/ServiceService.cs
+++ EldenBoost.Core/Services/ServiceService.cs
@@ -1,4 +1,9 @@
 ﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Models.Service;
+using EldenBoost.Core.Models.Service.Enums;
+using EldenBoost.Infrastructure.Data.Models;
+using EldenBoost.Infrastructure.Data.Repository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +14,52 @@ namespace EldenBoost.Core.Services
 {
     public class ServiceService : IServiceService
     {
+        private readonly IRepository repository;
+        public ServiceService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<AllServicesFilteredAndPagedModel> AllAsync(AllServicesQueryModel model)
+        {
+            IQueryable<Service> servicesQuery = repository.AllReadOnly<Service>()
+               .Where(s => s.IsActive)
+               .AsQueryable();
 
+            if (!string.IsNullOrWhiteSpace(model.SearchString))
+            {
+                string wildCard = $"%{model.SearchString.ToLower()}%";
+                servicesQuery = servicesQuery.Where(s => EF.Functions.Like(s.Title, wildCard) || EF.Functions.Like(s.Description, wildCard));
+            }
+
+            servicesQuery = model.ServiceSorting switch
+            {
+                ServiceSorting.Newest => servicesQuery.OrderByDescending(s => s.Id),
+                ServiceSorting.Oldest => servicesQuery.OrderBy(s => s.Id),
+                ServiceSorting.PriceAscending => servicesQuery.OrderBy(s => s.Price),
+                ServiceSorting.PriceDescending => servicesQuery.OrderByDescending(s => s.Price),
+                _ => throw new NotImplementedException()
+            };
+
+            IEnumerable<ServiceAllViewModel> allServices = await servicesQuery
+                .Skip((model.CurrentPage - 1) * model.ServicesPerPage)
+                .Take(model.ServicesPerPage)
+                .Select(s => new ServiceAllViewModel()
+                {
+                    Id = s.Id,
+                    Title = s.Title,
+                    Description = s.Description,
+                    ImageURL = s.ImageURL ?? string.Empty,
+                    Price = s.Price
+                })
+                .ToListAsync();
+
+            int totalServices = servicesQuery.Count();
+
+            return new AllServicesFilteredAndPagedModel()
+            {
+                TotalServicesCount = totalServices,
+                Services = allServices
+            };
+        }
     }
 }
