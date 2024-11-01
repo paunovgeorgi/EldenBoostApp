66f8e784377f0d1f740b4d1e543d4e65fe5de1bb
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 15:06:43 +0200

    ServicOptionVireModel created.

---
+++ EldenBoost.Core/Models/ServiceOption/ServiceOptionViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static EldenBoost.Common.Constants.ValidationConstants.ServiceOptionValidations;
+using static EldenBoost.Common.Constants.ValidationConstants.DecimalUniversalRange;
+using static EldenBoost.Common.Constants.ValidationErrors;
+
+namespace EldenBoost.Core.Models.ServiceOption
+{
+    public class ServiceOptionViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = FieldLength)]
+        public string Name { get; set; } = null!;
+
+        [Range(typeof(decimal), DecimalMin, DecimalMax, ErrorMessage = FieldLength)]
+        public decimal Price { get; set; }
+    }
+}
