2c570f2ee5e2e0772d03c6caa3b4a101847ef028
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 15:14:40 +0200

    ServiceAllViewModel created.

---
+++ EldenBoost.Core/Models/Service/ServiceAllViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EldenBoost.Common.Enumerations;
+using EldenBoost.Core.Models.ServiceOption;
+
+namespace EldenBoost.Core.Models.Service
+{
+    public class ServiceAllViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string ImageURL { get; set; } = null!;
+
+        public ServiceType ServiceType { get; set; }
+
+        public int MaxAmount { get; set; }
+
+        public ICollection<ServiceOptionViewModel> Options { get; set; } = new List<ServiceOptionViewModel>();
+    }
+}
