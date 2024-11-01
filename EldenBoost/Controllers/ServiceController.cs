c7f7efa57cc54c72365c34bb8bd55f4356638df8
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 18:11:06 +0200

    All Service view and All() ServiceController action implemented.

--- EldenBoost/Controllers/ServiceController.cs
+++ EldenBoost/Controllers/ServiceController.cs
@@ -1,6 +1,30 @@
-﻿namespace EldenBoost.Controllers
-{
-    public class ServiceController
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Models.Service;
+using EldenBoost.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Controllers
+{   
+    public class ServiceController : BaseController
     {
+        private readonly IServiceService serviceService;
+
+        public ServiceController(IServiceService _serviceService)
+        {
+            serviceService = _serviceService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> All([FromQuery] AllServicesQueryModel model)
+        {
+            AllServicesFilteredAndPagedModel serviceModel = await serviceService.AllAsync(model);
+
+            model.Services = serviceModel.Services;
+            model.TotalServices = serviceModel.TotalServicesCount;
+
+            return View(model);
+        }
     }
 }
