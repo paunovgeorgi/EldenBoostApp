908c8ad5a01da0ea2db5cad8436fccb60feb4610
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 15:44:26 +0200

    AllServicesFilteredAndPagedModel created.

---
+++ EldenBoost.Core/Models/Service/AllServicesFilteredAndPagedModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EldenBoost.Core.Models.Service
+{
+    public class AllServicesFilteredAndPagedModel
+    {
+        public int TotalServicesCount { get; set; }
+
+        public IEnumerable<ServiceAllViewModel> Services { get; set; } = new List<ServiceAllViewModel>();
+    }
+}
