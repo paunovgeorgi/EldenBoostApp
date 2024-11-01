426e1af95b551b8f9461387e475261ed0626457d
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 16:10:10 +0200

    GeneralApplicationConstants added to Common layer.

---
+++ EldenBoost.Common/Constants/GeneralApplicationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace EldenBoost.Common.Constants
+{
+    public static class GeneralApplicationConstants
+    {
+        public const int DefaultPage = 1;
+        public const int EntitiesPerPage = 4;
+    }
+}
