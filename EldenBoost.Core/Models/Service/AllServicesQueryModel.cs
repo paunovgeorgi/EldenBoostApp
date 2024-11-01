b10d63f5e3b850da2ea64c4cbcd3081d30ae9b16
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-01 16:18:20 +0200

    AllServiceQueryModel created.

---
+++ EldenBoost.Core/Models/Service/AllServicesQueryModel.cs
@@ -0,0 +1,24 @@
+﻿using EldenBoost.Core.Models.Service.Enums;
+using System.ComponentModel.DataAnnotations;
+using static EldenBoost.Common.Constants.GeneralApplicationConstants;
+
+namespace EldenBoost.Core.Models.Service
+{
+    public class AllServicesQueryModel
+    {
+        [Display(Name = "Search")]
+        public string? SearchString { get; set; }
+
+        [Display(Name = "Sort Services By")]
+        public ServiceSorting ServiceSorting { get; set; }
+
+        public int CurrentPage { get; set; } = DefaultPage;
+
+        [Display(Name = "Show Services On Page")]
+        public int ServicesPerPage { get; set; } = EntitiesPerPage;
+
+        public int TotalServices { get; set; }
+
+        public IEnumerable<ServiceAllViewModel> Services { get; set; } = new List<ServiceAllViewModel>();
+    }
+}
