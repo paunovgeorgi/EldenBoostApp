15cfcaa225e1072ca20b7cd84be9cb08fc79a064
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-02 18:28:56 +0200

    BottomSectionServices component created.

---
+++ EldenBoost/Components/BottomSectionServices.cs
@@ -0,0 +1,21 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class BottomSectionServices : ViewComponent
+    {
+        private readonly IServiceService serviceService;
+
+        public BottomSectionServices(IServiceService _serviceService)
+        {
+            serviceService = _serviceService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await serviceService.GetPopularServicesAsync();
+            return View(model);
+        }
+    }
+}
