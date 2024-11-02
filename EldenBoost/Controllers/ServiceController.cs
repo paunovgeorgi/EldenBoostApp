3ee02be2050858a9e0142efdfc76518c42013552
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-02 20:19:39 +0200

    ServiceController - Details Action - implemented.

--- EldenBoost/Controllers/ServiceController.cs
+++ EldenBoost/Controllers/ServiceController.cs
@@ -1,11 +1,13 @@
 ﻿using EldenBoost.Core.Contracts;
 using EldenBoost.Core.Models.Service;
-using EldenBoost.Core.Services;
+using EldenBoost.Core.Models.ServiceOption;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using EldenBoost.Core.Extensions;
+using static EldenBoost.Common.Constants.NotificationConstants;
 
 namespace EldenBoost.Controllers
-{   
+{
     public class ServiceController : BaseController
     {
         private readonly IServiceService serviceService;
@@ -32,6 +34,29 @@ namespace EldenBoost.Controllers
         public async Task<IActionResult> Popular()
         {
             var model = await serviceService.GetPopularServicesAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int id, string information)
+        {
+            var model = await serviceService.GetServiceViewModelByIdAsync(id);
+
+            if (model == null)
+            {
+                TempData[ErrorMessage] = "Service with provided model does not exist!";
+                return RedirectToAction("All", "Service");
+            }
+
+            if (information != model.GetInformation())
+            {
+                return BadRequest("Info doesn't add-up mate");
+            }
+
+            var options = await serviceService.GetServiceOptionsAsync(model.Id);
+            model.Options = (ICollection<ServiceOptionViewModel>)options;
 
             return View(model);
         }
