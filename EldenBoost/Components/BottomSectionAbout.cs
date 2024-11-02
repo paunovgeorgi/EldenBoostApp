78009c81bba5fc005b595cb0f883061494ec5572
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-02 18:29:13 +0200

    BottomSectionAbout component created.

---
+++ EldenBoost/Components/BottomSectionAbout.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class BottomSectionAbout : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
