3517e79dd4debedcf6e9b40a1d10cc5db5c9a979
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-02 19:07:37 +0200

    FaqComponent created.

---
+++ EldenBoost/Components/FaqComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class FaqComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
