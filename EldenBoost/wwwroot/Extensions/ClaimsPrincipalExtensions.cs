1946cfe981796ba5544d4d35f3d86f4757ac9314
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-02 10:50:49 +0200

    ClaimsPrincipalExtensions class created. Claims NameIdentifier method for user Id implemented.

---
+++ EldenBoost/wwwroot/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using static EldenBoost.Common.Constants.GeneralApplicationConstants;
+
+namespace EldenBoost.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
