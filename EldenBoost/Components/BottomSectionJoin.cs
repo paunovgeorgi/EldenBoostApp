cc35ca28ba348b3466d60688e1de729a312f8778
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-02 18:28:34 +0200

    BottomSectionJoin component created.

---
+++ EldenBoost/Components/BottomSectionJoin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class BottomSectionJoin : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
