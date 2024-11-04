0494345d980ffd506cd15d4be8180d213e4c872d
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-04 10:48:03 +0200

    ApplicationBuilderExtensions created. CreateAdminRoleAsync task implemented.

---
+++ EldenBoost/Extenstions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using EldenBoost.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using static EldenBoost.Common.Constants.GeneralApplicationConstants;
+
+namespace EldenBoost.Extenstions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task CreateAdminRoleAsync(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (userManager != null && roleManager != null && await roleManager.RoleExistsAsync(AdminRole) == false)
+            {
+                var role = new IdentityRole(AdminRole);
+                await roleManager.CreateAsync(role);
+
+                var admin = await userManager.FindByEmailAsync("admin@admin.com");
+
+                if (admin != null)
+                {
+                    await userManager.AddToRoleAsync(admin, role.Name!);
+                }
+            }
+        }
+    }
+}
