782aac1cc25305fadd23ce544ea32b36421683ad
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-04 15:22:03 +0200

    AuthorService created. ExistsByUserIdAsync task implemented.

---
+++ EldenBoost.Core/Contracts/IAuthorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Contracts
+{
+    public interface IAuthorService
+    {
+        Task<bool> ExistsByUserIdAsync(string userId);
+    }
+}
---
+++ EldenBoost.Core/Services/AuthorService.cs
@@ -0,0 +1,21 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Infrastructure.Data.Models;
+using EldenBoost.Infrastructure.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace EldenBoost.Core.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IRepository repository;
+        public AuthorService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task<bool> ExistsByUserIdAsync(string userId)
+        {
+            return await repository.AllReadOnly<Author>()
+                .AnyAsync(a => a.UserId == userId);
+        }
+    }
+}
