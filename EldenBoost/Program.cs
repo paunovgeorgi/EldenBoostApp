a6564018a05d1a692a61c739904feb3f629c2a14
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-04 19:17:07 +0200

    map.ControllerRourte for areas added.

--- EldenBoost/Program.cs
+++ EldenBoost/Program.cs
@@ -58,6 +58,12 @@ app.UseAuthorization();
 
 await app.CreateAdminRoleAsync();
 
+
+app.MapControllerRoute(
+     name: "areas",
+           pattern: "/{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
