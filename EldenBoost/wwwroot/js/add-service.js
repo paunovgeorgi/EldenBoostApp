a53a80f753832ca0fee545a09b8a995bab7e093d
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-05 18:15:44 +0200

    add-service javascript added for Add service action.

---
+++ EldenBoost/wwwroot/js/add-service.js
@@ -0,0 +1,17 @@
+﻿
+document.getElementById("ServiceType").addEventListener("change", function () {
+    var selectedType = this.value;
+    document.getElementById("slider-section").style.display = (selectedType == 1) ? 'block' : 'none'; 
+    document.getElementById("options-section").style.display = (selectedType == 2) ? 'block' : 'none';
+});
+
+
+document.getElementById("add-option-btn").addEventListener("click", function () {
+    var optionsContainer = document.getElementById("service-options-container");
+    var newOptionIndex = optionsContainer.children.length;
+    var newOptionField = `<div class="form-group">
+                                                      <input type="text" class="form-control" name="ServiceOptions[${newOptionIndex}].Name" placeholder="Option Name">
+                                                      <input type="number" step="0.01" class="form-control mt-2" name="ServiceOptions[${newOptionIndex}].Price" placeholder="Option Price">
+                                                  </div>`;
+    optionsContainer.insertAdjacentHTML('beforeend', newOptionField);
+});
