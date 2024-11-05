59dd9afd9212a35eb0a09d482e58114fa1907113
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-05 17:04:07 +0200

    HtmlHelperExtension implemented.

---
+++ EldenBoost/Extenstions/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EldenBoost.Extenstions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static SelectList EnumToSelectList<TEnum>(this IHtmlHelper htmlHelper) where TEnum : struct
+        {
+            if (!typeof(TEnum).IsEnum)
+                throw new ArgumentException("TEnum must be an enumerated type");
+
+            var enumValues = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+            var enumSelectList = enumValues.Select(e => new SelectListItem
+            {
+                Value = e.ToString(),
+                Text = e.ToString()
+            });
+
+            return new SelectList(enumSelectList, "Value", "Text");
+        }
+    }
+}
