34f16f361212800b74ee4386fe867fb8304362f3
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-05 18:15:12 +0200

    Unnecessary usings removed from IAuthorService.

--- EldenBoost.Core/Contracts/IAuthorService.cs
+++ EldenBoost.Core/Contracts/IAuthorService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EldenBoost.Core.Contracts
+﻿namespace EldenBoost.Core.Contracts
 {
     public interface IAuthorService
     {
