6aa91171cc1716f5a0e9cf592821801d73032e3d
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-05 15:26:31 +0200

    BaseAdminController created.

---
+++ EldenBoost/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,13 @@
+﻿using static EldenBoost.Common.Constants.GeneralApplicationConstants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Areas.Admin.Controllers
+{
+    [Area(AdminArea)]
+    [Authorize(Roles = AdminRole)]
+    public class BaseAdminController : Controller
+    {
+
+    }
+}
