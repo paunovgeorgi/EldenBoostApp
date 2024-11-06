11efb09cebb492741d61607fdaf1d12896a9f7cf
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-06 15:50:38 +0200

    OrderService created. CreateOrderAsync task implemented.

---
+++ EldenBoost.Core/Contracts/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Contracts
+{
+	public interface IOrderService
+	{
+		Task CreateOrderAsync(int serviceId, string clientId, int platformId, decimal? updatedPrice, bool? hasStream, bool? isExpress, int? optionId, int sliderValue);
+	}
+}
---
+++ EldenBoost.Core/Services/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Infrastructure.Data.Models;
+using EldenBoost.Infrastructure.Data.Repository;
+
+namespace EldenBoost.Core.Services
+{
+	public class OrderService : IOrderService
+	{
+		private readonly IRepository repository;
+        public OrderService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+        public async Task CreateOrderAsync(int serviceId, string clientId, int platformId, decimal? updatedPrice, bool? hasStream, bool? isExpress, int? optionId, int sliderValue)
+		{
+			var service = await repository.GetByIdAsync<Service>(serviceId);
+			string infoMessage = "";
+			ServiceOption option;
+			if (optionId != null)
+			{
+				option = await repository.GetByIdAsync<ServiceOption>(optionId);
+				infoMessage = $"Selected option: {option!.Name}";
+			}
+
+			if (service != null)
+			{
+				if (service.ServiceType == Common.Enumerations.ServiceType.Slider)
+				{
+					infoMessage = $"Selected amount: {sliderValue}";
+				}
+				var price = updatedPrice != null ? (decimal)updatedPrice : service.Price;
+				Order order = new Order()
+				{
+					Status = "Pending",
+					ServiceId = serviceId,
+					ClientId = clientId,
+					PlatformId = platformId,
+					TimeOfPurchase = DateTime.Now,
+					BoosterPay = price / 2,
+					Price = price,
+					HasStream = hasStream ?? false,
+					IsExpress = isExpress ?? false,
+					Information = infoMessage
+				};
+
+				service.PurchaseCount++;
+
+				await repository.AddAsync(order);
+				await repository.SaveChangesAsync();
+			}
+		}
+	}
+}
