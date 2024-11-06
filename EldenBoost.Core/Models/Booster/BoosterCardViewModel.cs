8a6c49bae87f4d97364e53b6b23167e006c6dabf
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-06 12:07:26 +0200

    BoosterCardViewModel created.

---
+++ EldenBoost.Core/Models/Booster/BoosterCardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Models.Booster
+{
+    public class BoosterCardViewModel
+    {
+        public string Name { get; set; } = null!;
+
+        public string? ProfilePicture { get; set; }
+
+        public string SupportedPlatforms { get; set; } = null!;
+
+        public double Rating { get; set; }
+
+        public double RatingCount { get; set; }
+    }
+}
