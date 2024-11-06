710b4609898287595e1e8b95c39d854d64c57cba
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-06 12:18:37 +0200

    AllBoostersToCardModelAsync Booster task implemented.

--- EldenBoost.Core/Contracts/IBoosterService.cs
+++ EldenBoost.Core/Contracts/IBoosterService.cs
@@ -1,7 +1,10 @@
-﻿namespace EldenBoost.Core.Contracts
+﻿using EldenBoost.Core.Models.Booster;
+
+namespace EldenBoost.Core.Contracts
 {
     public interface IBoosterService
     {
         Task<bool> BoosterExistsByUserIdAsync(string userId);
+        Task<IEnumerable<BoosterCardViewModel>> AllBoostersToCardModelAsync();
     }
 }
--- EldenBoost.Core/Services/BoosterService.cs
+++ EldenBoost.Core/Services/BoosterService.cs
@@ -1,4 +1,5 @@
 ﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Models.Booster;
 using EldenBoost.Infrastructure.Data.Models;
 using EldenBoost.Infrastructure.Data.Repository;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,23 @@ namespace EldenBoost.Core.Services
         {
             repository = _repository;
         }
+
+        public async Task<IEnumerable<BoosterCardViewModel>> AllBoostersToCardModelAsync()
+        {
+            return await repository.AllReadOnly<Booster>()
+               .Include(b => b.User)
+               .Include(b => b.Platforms)
+               .Select(b => new BoosterCardViewModel()
+               {
+                   Name = b.User.Nickname,
+                   ProfilePicture = b.User.ProfilePicture ?? string.Empty,
+                   SupportedPlatforms = string.Join(", ", b.Platforms.Select(p => p.Name).ToArray()),
+                   Rating = b.Rating,
+                   RatingCount = b.RatingCount
+               })
+               .ToListAsync();
+        }
+
         public async Task<bool> BoosterExistsByUserIdAsync(string userId)
         {
             return await repository.AllReadOnly<Booster>().AnyAsync(u => u.UserId == userId);
