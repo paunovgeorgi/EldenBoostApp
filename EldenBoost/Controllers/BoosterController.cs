7c75cfc4a9e39cfeafdb10dd8593e6db5b9c815b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-06 12:47:47 +0200

    All Boosters action and view implemented.

---
+++ EldenBoost/Controllers/BoosterController.cs
@@ -0,0 +1,24 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Controllers
+{
+    public class BoosterController : BaseController
+    {
+        private readonly IBoosterService boosterService;
+        public BoosterController(IBoosterService _boosterService)
+        {
+            boosterService = _boosterService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            var model = await boosterService.AllBoostersToCardModelAsync();
+
+            return View(model);
+        }
+    }
+}
