49cdcd1c8bcce0c5aa9c727c0cc3978ab5fa1932
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-06 19:34:25 +0200

    AllOrdersAsync, GetOrderByIdAsync tasks implemented.

--- EldenBoost.Core/Contracts/IOrderService.cs
+++ EldenBoost.Core/Contracts/IOrderService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using EldenBoost.Core.Models.Order;
+using EldenBoost.Infrastructure.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +12,7 @@ namespace EldenBoost.Core.Contracts
 	public interface IOrderService
 	{
 		Task CreateOrderAsync(int serviceId, string clientId, int platformId, decimal? updatedPrice, bool? hasStream, bool? isExpress, int? optionId, int sliderValue);
+        Task<IEnumerable<OrderCardViewModel>> AllOrdersAsync();
+		Task<Order?> GetOrderByIdAsync(int orderId);
 	}
 }
--- EldenBoost.Core/Services/OrderService.cs
+++ EldenBoost.Core/Services/OrderService.cs
@@ -1,6 +1,9 @@
 ﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Models.Order;
 using EldenBoost.Infrastructure.Data.Models;
 using EldenBoost.Infrastructure.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace EldenBoost.Core.Services
 {
@@ -11,6 +14,49 @@ namespace EldenBoost.Core.Services
         {
             repository = _repository;
         }
+
+        public async Task<IEnumerable<OrderCardViewModel>> AllOrdersAsync()
+        {
+            var orders = await repository.AllReadOnly<Order>()
+                .Where(o => o.IsArchived == false)
+                .Select(o => new OrderCardViewModel()
+                {
+                    Id = o.Id,
+                    PlatformId = o.PlatformId,
+                    PlatformName = o.Platform.Name,
+                    Status = o.Status,
+                    ServiceName = o.Service.Title,
+                    ImageURL = o.Service.ImageURL ?? string.Empty,
+                    BoosterPay = o.Price / 2,
+                    TimeOfPurchase = o.TimeOfPurchase.ToLongDateString(),
+                    HasStream = o.HasStream,
+                    IsExpress = o.IsExpress,
+                    Information = o.Information
+                })
+                .OrderByDescending(x => x.Id)
+                .ToListAsync();
+
+            foreach (var order in orders)
+            {
+                Order? currentOrder = await repository.GetByIdAsync<Order>(order.Id);
+
+                if (currentOrder!.BoosterId != null)
+                {
+                    Booster? booster = await repository.AllReadOnly<Booster>()
+                        .Include(b => b.User)
+                        .Where(b => b.Id == currentOrder.BoosterId)
+                        .FirstOrDefaultAsync();
+
+                    if (booster != null)
+                    {
+                        order.AssignedTo = booster.User.Nickname;
+                    }
+                }
+            }
+
+            return orders;
+        }
+
         public async Task CreateOrderAsync(int serviceId, string clientId, int platformId, decimal? updatedPrice, bool? hasStream, bool? isExpress, int? optionId, int sliderValue)
 		{
 			var service = await repository.GetByIdAsync<Service>(serviceId);
@@ -48,6 +94,11 @@ namespace EldenBoost.Core.Services
 				await repository.AddAsync(order);
 				await repository.SaveChangesAsync();
 			}
+		}
+
+		public async Task<Order?> GetOrderByIdAsync(int orderId)
+		{
+			return await repository.GetByIdAsync<Order>(orderId);
 		}
 	}
 }
