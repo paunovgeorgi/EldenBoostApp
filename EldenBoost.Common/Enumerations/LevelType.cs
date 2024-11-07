128e71d5d556fe55d359a329a236d8b0480c57dc
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-07 16:01:27 +0200

    LevelType enum implemented.

---
+++ EldenBoost.Common/Enumerations/LevelType.cs
@@ -0,0 +1,16 @@
+﻿namespace EldenBoost.Common.Enumerations
+{
+    public enum LevelType
+    {
+        Adventurer = 0,
+        Explorer = 1,
+        Pathfinder = 2,
+        Trailblazer = 3,
+        Vanguard = 4,
+        Hero = 5,
+        Champion = 6,
+        Conqueror = 7,
+        Legend = 8,
+        Myth = 9
+    }
+}
