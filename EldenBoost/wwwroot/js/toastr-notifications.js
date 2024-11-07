e7820e24c1e2efce5a91000e97f6dff9d8123d9c
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-07 17:29:34 +0200

    toastr notification script moved to a separate file due to loading problems.

---
+++ EldenBoost/wwwroot/js/toastr-notifications.js
@@ -0,0 +1,43 @@
+﻿var message = function () {
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    var showSuccess = function (message) {
+        toastr["success"](message)
+    }
+
+    var showError = function (message) {
+        toastr["error"](message)
+    }
+
+    var showWarning = function (message) {
+        toastr["warning"](message)
+    }
+
+    var showInfo = function (message) {
+        toastr["info"](message)
+    }
+
+    return {
+        showSuccess,
+        showError,
+        showInfo,
+        showWarning
+    }
+}();
