a2c5d70ef3f504eaf9df55d3d1e2920fe0b7d3d5
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-08 16:40:17 +0200

    AllArticlesQueryModel implemented.

---
+++ EldenBoost.Core/Models/Article/AllArticlesQueryModel.cs
@@ -0,0 +1,24 @@
+﻿using EldenBoost.Core.Models.Article.Enums;
+using System.ComponentModel.DataAnnotations;
+using static EldenBoost.Common.Constants.GeneralApplicationConstants;
+
+namespace EldenBoost.Core.Models.Article
+{
+    public class AllArticlesQueryModel
+    {
+        [Display(Name = "Search")]
+        public string? SearchString { get; set; }
+
+        [Display(Name = "Sort Articles By")]
+        public ArticleSorting ArticleSorting { get; set; }
+
+        public int CurrentPage { get; set; } = DefaultPage;
+
+        [Display(Name = "Show Articles On Page")]
+        public int ArticlesOnPage { get; set; } = EntitiesPerPage;
+
+        public int TotalArticles { get; set; }
+
+        public IEnumerable<ArticleCardViewModel> Articles { get; set; } = new List<ArticleCardViewModel>();
+    }
+}
