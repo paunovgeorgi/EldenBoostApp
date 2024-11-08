bcf0cbb15dd72e8f93a83e79444d3e9cfc931b6b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-08 17:50:11 +0200

    All articles action implemented.

---
+++ EldenBoost/Controllers/ArticleController.cs
@@ -0,0 +1,31 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Models.Article;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Controllers
+{
+    public class ArticleController : BaseController
+    {
+        private readonly IArticleService articleService;
+        private readonly IAuthorService authorService;
+
+        public ArticleController(IArticleService _articleService, IAuthorService _authorService)
+        {
+            articleService = _articleService;
+            authorService = _authorService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> All([FromQuery] AllArticlesQueryModel model)
+        {
+            AllArticlesFilteredAndPagedModel articleModel = await articleService.AllFilteredAndPagedAsync(model);
+
+            model.Articles = articleModel.Articles;
+            model.TotalArticles = articleModel.TotalArticleCount;
+
+            return View(model);
+        }
+    }
+}
