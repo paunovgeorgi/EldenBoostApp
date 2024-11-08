31decc704fe495dcf7bd7c2ff713af3531929a61
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-08 16:41:19 +0200

    AllArticlesFilteredAndPagedModel implemented.

---
+++ EldenBoost.Core/Models/Article/AllArticlesFilteredAndPagedModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EldenBoost.Core.Models.Article
+{
+    public class AllArticlesFilteredAndPagedModel
+    {
+        public int TotalArticleCount { get; set; }
+
+        public IEnumerable<ArticleCardViewModel> Articles { get; set; } = new List<ArticleCardViewModel>();
+    }
+}
