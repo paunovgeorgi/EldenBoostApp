4d26d8b7b9b59ab11ea56af5243c374622354621
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-08 16:32:49 +0200

    ArticleCardViewModel implemented.

---
+++ EldenBoost.Core/Models/Article/ArticleCardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EldenBoost.Common.Enumerations;
+using EldenBoost.Core.Models.Article.Contracts;
+using System.ComponentModel.DataAnnotations;
+
+namespace EldenBoost.Core.Models.Article
+{
+    public class ArticleCardViewModel : IArticleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        [Display(Name = "Article Type")]
+        public ArticleType ArticleType { get; set; }
+
+        public string ImageURL { get; set; } = null!;
+    }
+}
