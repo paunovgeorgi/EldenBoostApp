d10898af69448d4e434408a56da89823cd5578f0
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-10 20:02:33 +0200

    MyProfile AuthorController action implemented.

---
+++ EldenBoost/Controllers/AuthorController.cs
@@ -0,0 +1,32 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Extensions;
+using EldenBoost.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Controllers
+{
+    public class AuthorController : BaseController
+    {
+        private readonly IAuthorService authorService;
+        private readonly IArticleService articleService;
+
+        public AuthorController(IAuthorService _authorService, IArticleService _articleService)
+        {
+            authorService = _authorService;
+            articleService = _articleService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> MyProfile()
+        {
+            Author? author = await authorService.GetAuthorByUserIdAsync(User.Id());
+            if (author == null)
+            {
+                return Unauthorized("You don't have access to this page!");
+            }
+
+            var articles = await articleService.AllByAuthorIdAsync(author.Id);
+
+            return View(articles);
+        }
+    }
+}
