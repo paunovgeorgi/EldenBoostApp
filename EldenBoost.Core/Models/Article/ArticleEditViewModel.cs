258390dbe5ccb0c636dd809dbab4bbd234ce917a
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-11 19:41:19 +0200

    ArticleEditViewModel implemented.

---
+++ EldenBoost.Core/Models/Article/ArticleEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using EldenBoost.Common.Enumerations;
+using System.ComponentModel.DataAnnotations;
+using static EldenBoost.Common.Constants.ValidationConstants.ArticleValidations;
+
+namespace EldenBoost.Core.Models.Article
+{
+    public class ArticleEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength)]
+        public string Content { get; set; } = null!;
+
+        [Display(Name = "Article Type")]
+        public ArticleType ArticleType { get; set; }
+
+        [Required]
+        [MaxLength(ImageURLMaxLength)]
+        public string ImageURL { get; set; } = null!;
+    }
+}
