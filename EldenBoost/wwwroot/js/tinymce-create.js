e0ccef4ab5559486c8772c44b2ccdbdc7f11b5a0
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-11 22:31:56 +0200

    tinimce edit and create scripts implemented.

---
+++ EldenBoost/wwwroot/js/tinymce-create.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    tinymce.init({
+        selector: '#contentTextArea',
+        statusbar: false,
+        height: 500,
+        menubar: true,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar: 'code | undo redo | formatselect | bold italic backcolor | \
+                          alignleft aligncenter alignright alignjustify | \
+                          bullist numlist outdent indent | removeformat | help'
+    });
+});
---
+++ EldenBoost/wwwroot/js/tinymce-edit.js
@@ -0,0 +1,18 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    tinymce.init({
+        selector: '#contentTextArea',
+        content_css: 'dark',
+        statusbar: false,
+        height: 500,
+        menubar: true,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+        ],
+        toolbar: 'code | undo redo | formatselect | bold italic backcolor | \
+                              alignleft aligncenter alignright alignjustify | \
+                              bullist numlist outdent indent | removeformat | help'
+    });
+});
