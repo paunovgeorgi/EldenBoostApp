09b5efa850eaa96990e79d37b1106bc5831391e6
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-11 15:21:14 +0200

    ArticleReadViewModel implemented (model for Article Read view)

---
+++ EldenBoost.Core/Models/Article/ArticleReadViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EldenBoost.Core.Models.Article
+{
+	public class ArticleReadViewModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = null!;
+
+		public string Content { get; set; } = null!;
+
+		public string ImgUrl { get; set; } = string.Empty;
+
+		[Display(Name = "Published On:")]
+		public string PublishDate { get; set; } = null!;
+
+		public string Author { get; set; } = null!;
+	}
+}
