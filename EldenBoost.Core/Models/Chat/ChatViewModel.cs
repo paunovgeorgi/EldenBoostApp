4e428c69a4cb58bfb6f77cb01bd39015c9cc9b17
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-12 16:05:23 +0200

    ChatViewModel implemented.

---
+++ EldenBoost.Core/Models/Chat/ChatViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EldenBoost.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Models.Chat
+{
+    public class ChatViewModel
+    {
+        public int OrderId { get; set; }
+        public string ReceiverId { get; set; } = null!;
+        public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+    }
+}
