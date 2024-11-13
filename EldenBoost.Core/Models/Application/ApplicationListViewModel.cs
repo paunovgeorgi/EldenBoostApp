7c71ca1ee4e9ac723d583a60cb15d3ee41084888
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-13 14:53:59 +0200

    ApplicationListViewModel for displaying application data in admin panel - implemented.

---
+++ EldenBoost.Core/Models/Application/ApplicationListViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace EldenBoost.Core.Models.Application
+{
+    public class ApplicationListViewModel
+    {
+        public int Id { get; set; }
+        public string Country { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Nickname { get; set; } = null!;
+        public int Availability { get; set; }
+        public string Experience { get; set; } = null!;
+        public string Platforms { get; set; } = null!;
+        public bool IsApproved { get; set; }
+        public bool IsRejected { get; set; }
+    }
+
+}
