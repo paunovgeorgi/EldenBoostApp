b97da8f393f766aabd83fc6829dfd80ae5ecc944
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-13 14:55:19 +0200

    ApplicationController in Admin Area created. AllBooster action implemented.

---
+++ EldenBoost/Areas/Admin/Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using EldenBoost.Common.Enumerations;
+using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Areas.Admin.Controllers
+{
+    public class ApplicationController : BaseAdminController
+    {
+        private readonly IApplicationService applicationService;
+
+        public ApplicationController(IApplicationService _applicationService)
+        {
+            applicationService = _applicationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllBooster()
+        {
+            var models = await applicationService.AllAsync(a => !a.IsApproved && !a.IsRejected && a.ApplicationType == ApplicationType.Booster);
+            return View(models);
+        }
+    }
+}
