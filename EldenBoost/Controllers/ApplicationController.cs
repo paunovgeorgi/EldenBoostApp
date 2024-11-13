fa30d9a731c7a5f988b71bf6e5e81637e17e1885
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-13 10:48:17 +0200

    SubmitAuthor action implemented / additional checks added to SubmitBooster action.

--- EldenBoost/Controllers/ApplicationController.cs
+++ EldenBoost/Controllers/ApplicationController.cs
@@ -1,6 +1,7 @@
 ﻿using EldenBoost.Common.Enumerations;
 using EldenBoost.Core.Contracts;
 using EldenBoost.Core.Models.Application;
+using EldenBoost.Core.Services;
 using EldenBoost.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using static EldenBoost.Common.Constants.NotificationConstants;
@@ -14,18 +15,21 @@ namespace EldenBoost.Controllers
         private readonly IOrderService orderService;
         private readonly IBoosterService boosterService;
         private readonly IUserService userService;
+        private readonly IAuthorService authorService;
 
         public ApplicationController(IApplicationService _applicationService,
             IPlatformService _platformService,
             IOrderService _orderService,
             IBoosterService _boosterService,
-            IUserService _userService)
+            IUserService _userService,
+            IAuthorService _authorService)
         {
             applicationService = _applicationService;
             platformService = _platformService;
             orderService = _orderService;
             boosterService = _boosterService;
             userService = _userService;
+            authorService = _authorService;
         }
 
         [HttpGet]
@@ -56,6 +60,31 @@ namespace EldenBoost.Controllers
                 return RedirectToAction("Index", "Home");
             }
 
+            bool isBooster = await boosterService.BoosterExistsByUserIdAsync(userId);
+            if (isBooster)
+            {
+                TempData[InformationMessage] = "You are already part of our Boosters team!";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool isAuthor = await authorService.ExistsByUserIdAsync(userId);
+            if (isAuthor)
+            {
+                TempData[InformationMessage] = "You are already part of our Authors team!";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+
             var platforms = await platformService.GetAllPlatformsAsync();
 
             var model = new ApplicationFormModel();
@@ -75,6 +104,103 @@ namespace EldenBoost.Controllers
 
             TempData[SuccessMessage] = "Successfully submited your application. Our admins will contact you if approved!";
             await applicationService.CreateBoosterApplicationAsync(User.Id(), model);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SubmitAuthor()
+        {
+
+            string userId = User.Id();
+
+            if (await userService.HasOrdersAsync(userId))
+            {
+                TempData[InformationMessage] = "You are already using our service as a client. If you'd like to apply for this position you should make a new registration.";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool hasApplied = await applicationService.HasAppliedByUserIdAsync(userId, a => a.ApplicationType == ApplicationType.Author);
+            if (hasApplied)
+            {
+                TempData[InformationMessage] = "You've already applied for this position!";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool isBooster = await boosterService.BoosterExistsByUserIdAsync(userId);
+            if (isBooster)
+            {
+                TempData[InformationMessage] = "You are already part of our Boosters team!";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool isAuthor = await authorService.ExistsByUserIdAsync(userId);
+            if (isAuthor)
+            {
+                TempData[InformationMessage] = "You are already part of our Authors team!";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            var model = new ApplicationFormModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubmitAuthor(ApplicationFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            string userId = User.Id();
+
+            if (await userService.HasOrdersAsync(userId))
+            {
+                TempData[InformationMessage] = "You are already using our service as a client. If you'd like to apply for this position you should make a new registration.";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool hasApplied = await applicationService.HasAppliedByUserIdAsync(userId, a => a.ApplicationType == ApplicationType.Author);
+            if (hasApplied)
+            {
+                TempData[InformationMessage] = "You've already applied for this position!";
+                var refererUrl = Request.Headers["Referer"].ToString();
+                if (!string.IsNullOrEmpty(refererUrl))
+                {
+                    return Redirect(refererUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            await applicationService.CreateAuthorApplicationAsync(User.Id(), model);
+            TempData[SuccessMessage] = "Successfully submited your application. Our admins will contact you if approved!";
 
             return RedirectToAction("Index", "Home");
         }
