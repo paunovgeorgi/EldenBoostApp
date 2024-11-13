bdb39ea4963cb2189866f9b02be01dcb82f41a3e
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-13 15:32:31 +0200

    ApplicationApiController created. GetApproved and GetPending actions implemented.

---
+++ EldenBoost.WebApi/Controllers/ApplicationApiController.cs
@@ -0,0 +1,49 @@
+﻿using EldenBoost.Common.Enumerations;
+using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.WebApi.Controllers
+{
+    [Route("api/application")]
+    [ApiController]
+    public class ApplicationApiController : Controller
+    {
+        private readonly IApplicationService applicationService;
+
+        public ApplicationApiController(IApplicationService _applicationService)
+        {
+            applicationService = _applicationService;
+        }
+
+        [HttpGet("get-approved")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetApproved()
+        {
+            try
+            {
+                var applications = await applicationService.AllAsync(a => a.IsApproved && a.ApplicationType == ApplicationType.Booster);
+                return Ok(applications);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("get-pending")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetPending()
+        {
+            try
+            {
+                var applications = await applicationService.AllAsync(a => !a.IsApproved && !a.IsRejected && a.ApplicationType == ApplicationType.Booster);
+                return Ok(applications);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
