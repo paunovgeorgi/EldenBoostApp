8a83bb0c0d46b29231ab0135dc5608129d2409b8
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-14 10:17:05 +0200

    OrderApiController created / GetArchived and GetPending actions implemented.

---
+++ EldenBoost.WebApi/Controllers/OrderApiController.cs
@@ -0,0 +1,51 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.WebApi.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderApiController : Controller
+    {
+        private readonly IOrderService orderService;
+        public OrderApiController(IOrderService _orderService)
+        {
+            orderService = _orderService;
+        }
+
+        [HttpGet("getArchived")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetArchived()
+        {
+            try
+            {
+                var orders = await orderService.AllOrdersFilteredAsync(o => o.IsArchived);
+
+                return Ok(orders);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet("get-pending")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetPending()
+        {
+           
+            try
+            {
+                var result = await orderService.AllOrdersAsync();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
