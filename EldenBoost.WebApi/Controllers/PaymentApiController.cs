31d77ea0f89e0947caf862326b3059915acd04c6
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-14 17:51:42 +0200

    PaymentApiController created / GetPaid, GetPending actions implemented.

---
+++ EldenBoost.WebApi/Controllers/PaymentApiController.cs
@@ -0,0 +1,52 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.WebApi.Controllers
+{
+    [Route("api/payment")]
+    [ApiController]
+    public class PaymentApiController : Controller
+    {
+        private readonly IPaymentService paymentService;
+
+        public PaymentApiController(IPaymentService _paymentService)
+        {
+            paymentService = _paymentService;
+        }
+
+        [HttpGet("getPaid")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetPaid()
+        {
+            try
+            {
+                var payments = await paymentService.AllPaymentsFiltered(p => p.IsPaid);
+
+                return Ok(payments);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet("getPending")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetPending()
+        {
+            try
+            {
+                var payments = await paymentService.AllPaymentsFiltered(p => !p.IsPaid);
+
+                return Ok(payments);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
