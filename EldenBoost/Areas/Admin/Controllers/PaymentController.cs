5c192f8511d0e2ba6d0cd344878b0513cc648437
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-14 18:08:33 +0200

    Admin Area PaymentController created, All payments action implemented.

---
+++ EldenBoost/Areas/Admin/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Areas.Admin.Controllers
+{
+    public class PaymentController : BaseAdminController
+    {
+        private readonly IPaymentService paymentService;
+
+        public PaymentController(IPaymentService _paymentService)
+        {
+            paymentService = _paymentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var payments = await paymentService.AllPaymentsFiltered(p => p.IsPaid == false);
+
+            return View(payments);
+        }
+    }
+}
