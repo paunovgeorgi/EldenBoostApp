cfae8dd25e57966be8a85cabad47bb94674e0dcb
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-15 17:10:41 +0200

    OrderDetailsViewModel implemented.

---
+++ EldenBoost.Core/Models/Order/OrderDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EldenBoost.Core.Models.Order
+{
+    public class OrderDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string ServiceName { get; set; } = null!;
+
+        public decimal BoosterPay { get; set; }
+
+        public string ImageURL { get; set; } = null!;
+
+        public bool HasStream { get; set; }
+
+        public bool IsExpress { get; set; }
+
+        public string Platform { get; set; } = null!;
+    }
+}
