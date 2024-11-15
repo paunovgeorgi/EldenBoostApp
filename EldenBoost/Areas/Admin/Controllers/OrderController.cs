8b3834b3e48445874ce9d1fa04ba5cb97e04beb2
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-15 17:11:38 +0200

    Admin Area OrderController created. GetOrderDetails action implemented.

---
+++ EldenBoost/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Areas.Admin.Controllers
+{
+    public class OrderController : BaseAdminController
+    {
+        private readonly IOrderService orderService;
+
+        public OrderController(IOrderService _orderService)
+        {
+            orderService = _orderService;
+        }
+
+      
+        [HttpGet]
+        public async Task<IActionResult> GetOrderDetails(int orderId)
+        {
+            var order = await orderService.GetOrderDetailsAsync(orderId); // Fetch order details from the service
+            if (order == null)
+            {
+                return NotFound("Order not found");
+            }
+            return PartialView("_OrderDetailsPartial", order);
+        }
+    }
+}
