d0c57db3d12e71195773ea25d7a83e06eeb13657
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-15 17:57:20 +0200

    order-details-modal js function implemented.

---
+++ EldenBoost/wwwroot/js/order-details-modal.js
@@ -0,0 +1,22 @@
+﻿
+document.addEventListener('DOMContentLoaded', function () {
+    const modalElement = document.getElementById('orderDetailsModal');
+    const modal = new bootstrap.Modal(modalElement);
+    const orderLinks = document.querySelectorAll('.order-link');
+
+    orderLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const orderId = this.getAttribute('data-order-id');
+
+            fetch(`/Admin/Order/GetOrderDetails?orderId=${orderId}`)
+                .then(response => response.text())
+                .then(html => {
+                    document.getElementById('orderDetailsContent').innerHTML = html;
+                    modal.show();
+                })
+                .catch(error => console.error('Error fetching order details:', error));
+        });
+    });
+});
