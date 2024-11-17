4d7ba051d13e2304e9488703caff063915a230a1
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-17 16:36:30 +0200

    Cart model entity created.

---
+++ EldenBoost.Infrastructure/Data/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Client))]
+        public string ClientId { get; set; } = null!;
+        public ApplicationUser Client { get; set; } = null!;
+
+        public ICollection<CartItem> CartItems { get; set; } = new HashSet<CartItem>();
+    }
+}
