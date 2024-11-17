9100a75fa219aee5f4d3c5e230f91474b24a1e21
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-17 16:36:15 +0200

    CartItem model entity created.

---
+++ EldenBoost.Infrastructure/Data/Models/CartItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Infrastructure.Data.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Cart))]
+        public int CartId { get; set; }
+        public Cart Cart { get; set; } = null!;
+
+        [ForeignKey(nameof(Service))]
+        public int ServiceId { get; set; }
+        public Service Service { get; set; } = null!;
+
+        public int PlatformId { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public bool? HasStream { get; set; }
+
+        public bool? IsExpress { get; set; }
+
+        public int? OptionId { get; set; }
+
+        public int SliderValue { get; set; }
+        public string? Information { get; set; }
+    }
+}
