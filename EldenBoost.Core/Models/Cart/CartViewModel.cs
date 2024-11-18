56c8783fc4ec8d5236963d5ca9d59393cf83357a
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-18 15:56:35 +0200

    CartViewModel and CartItemViewModel implemented.

---
+++ EldenBoost.Core/Models/Cart/CartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EldenBoost.Core.Models.CartItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Models.Cart
+{
+    public class CartViewModel
+    {
+        public IEnumerable<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
+        public decimal TotalPrice { get; set; } 
+    }
+}
---
+++ EldenBoost.Core/Models/CartItem/CartItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EldenBoost.Core.Models.CartItem
+{
+    public class CartItemViewModel
+    {
+        public int Id { get; set; }
+        public string ServiceTitle { get; set; } = null!;
+        public string Platform { get; set; } = null!;
+        public decimal Price { get; set; } 
+        public string? Information { get; set; } 
+        public bool? HasStream { get; set; } 
+        public bool? IsExpress { get; set; }
+        public string ServiceImage { get; set; } = null!;
+    }
+}
