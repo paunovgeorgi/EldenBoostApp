6db384c416d5da086dd433694b290299417dd711
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-19 21:08:27 +0200

    service-toggle js script implemented.

---
+++ EldenBoost/wwwroot/js/service-toggle.js
@@ -0,0 +1,58 @@
+﻿
+
+let filterBtn = document.querySelector('#show-deactivated');
+let isShowingDeactivated = false;
+
+filterBtn.addEventListener('click', toggleServices);
+
+async function toggleServices() {
+    const tableBody = document.querySelector('table tbody');
+
+    let url = 'https://localhost:7277/api/service/';
+    if (isShowingDeactivated) {
+        url += 'getActive';
+    } else {
+        url += 'getDeactivated';
+    }
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error fetching orders: ${response.statusText}`);
+        }
+        const data = await response.json();
+
+
+        tableBody.innerHTML = '';
+
+
+        data.forEach(service => {
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+                <td><label>${service.title}</label></td>
+                <td><label>${service.price}</label></td>
+                <td><label>${service.purchaseCount}</label></td>
+                <td>
+                   ${service.isActive ? `<form method="post" action="/Service/Deactivate/${service.id}" style="display: inline">
+                        <button type="submit" class="btn btn-outline-danger">Deactivate</button>
+                    </form>` : `<form method="post" action="/Service/Activate/${service.id}" style="display: inline">
+                        <button type="submit" class="btn btn-outline-success">Activate</button>
+                    </form>`}
+                </td>
+                <td>
+                    <a href="/Service/Edit/${service.id}?area="Admin" class="btn btn-outline-light">Edit</a>
+                </td>
+            `;
+
+            tableBody.appendChild(row);
+        });
+
+
+        isShowingDeactivated = !isShowingDeactivated;
+        filterBtn.textContent = isShowingDeactivated ? 'Show Active' : 'Show Deactivated';
+    } catch (error) {
+        console.error(error);
+    }
+}
+
