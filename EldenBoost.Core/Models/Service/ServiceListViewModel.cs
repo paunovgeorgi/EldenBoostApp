9d710b1a2e057bdc2ea85c79e6fdbbecbef463ff
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-19 17:47:58 +0200

    ServiceListViewModel implemented.

---
+++ EldenBoost.Core/Models/Service/ServiceListViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EldenBoost.Core.Models.Service
+{
+    public class ServiceListViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int PurchaseCount { get; set; }
+    }
+}
