029e3157b24a4bc4969f8b886fdce6f707a25fc4
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-20 19:17:56 +0200

    Admin ArticleController created. All action and view implemented.

---
+++ EldenBoost/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,19 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Areas.Admin.Controllers
+{
+    public class ArticleController : BaseAdminController
+    {
+        private readonly IArticleService articleService;
+        public ArticleController(IArticleService _articleService)
+        {
+            articleService = _articleService;
+        }
+        public async Task<IActionResult> All()
+        {
+            var articles = await articleService.GetAllArticlesListViewModelAsync();
+            return View(articles);
+        }
+    }
+}
