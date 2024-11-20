eb16cc4aa3b3f9ced981033cd7f43733e77b669b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-20 11:52:03 +0200

    Admin UserController created / All action, All view implemented.

---
+++ EldenBoost/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Areas.Admin.Controllers
+{
+    public class UserController : BaseAdminController
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var users = await userService.AllAsync();
+            return View(users);
+        }
+    }
+}
