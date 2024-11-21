72c16730c98cb9accdc54412a66b850bac5d15a7
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-21 19:57:41 +0200

    ReviewsCarouselComponent implemented.

---
+++ EldenBoost/Components/ReviewCarouselComponent.cs
@@ -0,0 +1,21 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class ReviewsCarouselComponent : ViewComponent
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewsCarouselComponent(IReviewService _reviewService)
+        {
+            reviewService = _reviewService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await reviewService.GetLatestReviewsAsync();
+            return View(model);
+        }
+    }
+}
