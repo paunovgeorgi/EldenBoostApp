5184f8db89e15b04c4119e786f838eaa743eab52
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-21 19:58:04 +0200

    review-card-modal js script for ReviewsCarouselComponent implemented.

---
+++ EldenBoost/wwwroot/js/review-card-modal.js
@@ -0,0 +1,22 @@
+﻿
+document.addEventListener('DOMContentLoaded', function () {
+    var reviewModal = document.getElementById('reviewModal');
+    reviewModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget; // Button that triggered the modal
+        var nickname = button.getAttribute('data-nickname');
+        var reviewDate = button.getAttribute('data-reviewdate');
+        var content = button.getAttribute('data-content');
+        var profilePicture = button.getAttribute('data-profilepicture');
+
+        // Update the modal's content with the clicked review details
+        var modalNickname = document.getElementById('modalNickname');
+        var modalReviewDate = document.getElementById('modalReviewDate');
+        var modalContent = document.getElementById('modalContent');
+        var modalProfilePicture = document.getElementById('modalProfilePicture');
+
+        modalNickname.textContent = nickname;
+        modalReviewDate.textContent = reviewDate;
+        modalContent.textContent = content;
+        modalProfilePicture.src = profilePicture;
+    });
+});
