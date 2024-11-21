5825c9d993077ca231b5225e56f77f0a3ded0b93
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-21 19:56:29 +0200

    ReviewCardViewModel implemented.

---
+++ EldenBoost.Core/Models/Review/ReviewCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EldenBoost.Core.Models.Review
+{
+    public class ReviewCardViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nickname { get; set; } = null!;
+
+        public string ProfilePicture { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public string ReviewDate { get; set; } = null!;
+    }
+}
