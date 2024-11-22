63960966842a076faaa14d6be7a9d600dd928a5f
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-22 15:23:21 +0200

    NewsCarouselComponent implemented.

---
+++ EldenBoost/Components/NewsCarouselComponent.cs
@@ -0,0 +1,20 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class NewsCarouselComponent : ViewComponent
+    {
+        private readonly IArticleService articleService;
+
+        public NewsCarouselComponent(IArticleService _articleService)
+        {
+            articleService = _articleService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await articleService.LastThreeArticlesAsync(a => a.ArticleType == Common.Enumerations.ArticleType.News);
+            return View(model);
+        }
+    }
+}
