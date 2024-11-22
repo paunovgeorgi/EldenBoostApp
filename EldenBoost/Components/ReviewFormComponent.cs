40ec305758dd6f661b8575c50f5444042f48aade
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-22 16:53:57 +0200

    ReviewFormComponent implemented and added to Index / review-button scroll down script implemented.

---
+++ EldenBoost/Components/ReviewFormComponent.cs
@@ -0,0 +1,15 @@
+﻿using EldenBoost.Core.Models.Review;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class ReviewFormComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ReviewFormViewModel? model = null)
+        {
+            // Provide a new model if none is passed
+            model ??= new ReviewFormViewModel();
+            return View(model);
+        }
+    }
+}
---
+++ EldenBoost/wwwroot/js/homepage-scroll.js
@@ -0,0 +1,18 @@
+﻿
+document.querySelectorAll('a.scroll-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Get the target element
+        const target = document.querySelector(this.getAttribute('href'));
+
+        // Get the position of the target and subtract the offset
+        const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - 80; // 40px offset
+
+        // Smooth scroll to the target position
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
