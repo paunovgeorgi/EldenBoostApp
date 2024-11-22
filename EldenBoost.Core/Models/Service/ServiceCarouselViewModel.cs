3ac7b81053d14b55a69e54df4028f305d8c98640
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-22 15:24:04 +0200

    ServiceCarouselViewModel implemented.

---
+++ EldenBoost.Core/Models/Service/ServiceCarouselViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EldenBoost.Core.Models.Service.Contracts;
+
+namespace EldenBoost.Core.Models.Service
+{
+    public class ServiceCarouselViewModel : IServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string ImageURL { get; set; } = null!;
+    }
+}
