b5672666ff357177c0a9b654f9cee3da8ce3a15b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-22 15:24:54 +0200

    ServiceCarouselComponent implemented.

---
+++ EldenBoost/Components/ServicesCarouselComponent.cs
@@ -0,0 +1,20 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class ServicesCarouselComponent : ViewComponent
+    {
+        private readonly IServiceService serviceService;
+
+        public ServicesCarouselComponent(IServiceService _serviceService)
+        {
+            serviceService = _serviceService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await serviceService.LastThreeServicesAsync();
+            return View(model);
+        }
+    }
+}
