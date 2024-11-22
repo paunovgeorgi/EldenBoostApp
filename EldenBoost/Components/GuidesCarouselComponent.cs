bb30747e2d326ee71847c9e8a877bafb92127411
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-22 15:34:47 +0200

    GuidesCarouselComponent implemented.

---
+++ EldenBoost/Components/GuidesCarouselComponent.cs
@@ -0,0 +1,20 @@
+﻿using EldenBoost.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EldenBoost.Components
+{
+    public class GuidesCarouselComponent : ViewComponent
+    {
+        private readonly IArticleService articleService;
+
+        public GuidesCarouselComponent(IArticleService _articleService)
+        {
+            articleService = _articleService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await articleService.LastThreeArticlesAsync(a => a.ArticleType == Common.Enumerations.ArticleType.Guide);
+            return View(model);
+        }
+    }
+}
