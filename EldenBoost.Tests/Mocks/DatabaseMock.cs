1fbbcbdd905fd848b7ae4a4c8ca493ed591e3f08
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-24 16:04:26 +0200

    EldenBoost.Tests NUnit project created. DatabaseMock class implemented.

---
+++ EldenBoost.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,26 @@
+﻿using EldenBoost.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static EldenBoostDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<EldenBoostDbContext>()
+                    .UseInMemoryDatabase("HouseRentingInMemoryDb" + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new EldenBoostDbContext(dbContextOptions);
+            }
+
+        }
+    }
+}
