89f9e5006bc130bcb7dd327517e48e94ed7af9e6
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-24 16:20:46 +0200

    More UserService tests implemented.

--- EldenBoost.Tests/UnitTests/UserServiceTests.cs
+++ EldenBoost.Tests/UnitTests/UserServiceTests.cs
@@ -28,12 +28,20 @@ namespace EldenBoost.Tests.UnitTests
             userService = new UserService(repository,boosterService, authorService, orderService);
         }
 
+        [Test]
         public void AllAsync_ShouldReturn3Users()
         {
             int expectedCount = 3;
             var users = userService.AllAsync().Result;
 
             Assert.That(users.Count(), Is.EqualTo(expectedCount));
+        }
+
+        [Test]
+        public void AllAsync_ReturnListShouldContainUser()
+        {
+            var users = userService.AllAsync().Result;
+            Assert.That(users.Any(u => u.Nickname == User.Nickname));
         }
 
         [Test]
@@ -45,11 +53,41 @@ namespace EldenBoost.Tests.UnitTests
         }
 
         [Test]
+        public void GetProfilePictureAsync_ShouldRetrieveIncorrectProfilePicture()
+        {
+            string expectedPic = "WrongPicture";
+            string retrievedPic = userService.GetProfilePictureByUseIdAsync(User.Id).Result;
+            Assert.That(expectedPic, Is.Not.SameAs(retrievedPic));
+        }
+
+        [Test]
         public void ChangeProfilePicutre_ShouldWork()
         {
             string expectedPic = "NewClientPicture";
             userService.ChangeProfilePictureAsync(User.Id, expectedPic);
             Assert.That(User.ProfilePicture, Is.EqualTo(expectedPic));
+        }
+
+        [Test]
+        public void GetUserNicknameAsync_ShouldReturnCorrectNickname()
+        {
+            string expected = User2.Nickname;
+            string retrieved = userService.GetUserNicknameAsync("BoosterUserId").Result;
+            Assert.That(expected, Is.EqualTo(retrieved));
+        }
+
+        [Test]
+        public void HasOrdersAsync_ShouldReturn_True()
+        {
+            bool result = userService.HasOrdersAsync(User.Id).Result;
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void HasOrdersAsync_ShouldReturn_False()
+        {
+            bool result = userService.HasOrdersAsync(User2.Id).Result;
+            Assert.IsFalse(result);
         }
     }
 }
