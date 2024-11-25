a598efb7e6fe772ed7fbea977a7b2fc97ed84b9e
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-25 16:04:15 +0200

    ServiceServiceTests refactored for asynchronous testing. Arrange, Act, Assert, ClearnUp comment lines added for more clarity. More tests implemented.

--- EldenBoost.Tests/UnitTests/ServiceServiceTests.cs
+++ EldenBoost.Tests/UnitTests/ServiceServiceTests.cs
@@ -4,8 +4,10 @@ using EldenBoost.Core.Models.Service;
 using EldenBoost.Core.Services;
 using EldenBoost.Infrastructure.Data.Models;
 using EldenBoost.Infrastructure.Data.Repository;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,7 @@ namespace EldenBoost.Tests.UnitTests
     {
         private IRepository repository;
         private IServiceService serviceService;
+
         [OneTimeSetUp]
         public void SetUp()
         {
@@ -27,14 +30,20 @@ namespace EldenBoost.Tests.UnitTests
         [Test]
         public void ActivateByIdAsync_SetsIsActive_ToTrue()
         {
+            // Arrange
             Service.IsActive = false;
+
+            // Act
             serviceService.ActivateByIdAsync(Service.Id);
+
+            // Assert
             Assert.IsTrue(Service.IsActive);
         }
 
         [Test]
         public void AllAsync_ReturnsCorrect_NumberOfServices()
         {
+            // Arrange
             int expected = 2;
             var model = new AllServicesQueryModel
             {
@@ -44,13 +53,18 @@ namespace EldenBoost.Tests.UnitTests
                 TotalServices = 2,
                 ServiceSorting = Core.Models.Service.Enums.ServiceSorting.Newest,
             };
+
+            // Act
             var services = serviceService.AllAsync(model).Result;
+
+            // Assert
             Assert.That(expected, Is.EqualTo(services.TotalServicesCount));
         }
 
         [Test]
         public void AllAsync_SearchString_ReturnsCorrectServices()
         {
+            // Arrange
             int expected = 1;
             var model = new AllServicesQueryModel
             {
@@ -60,21 +74,31 @@ namespace EldenBoost.Tests.UnitTests
                 TotalServices = 1,
                 ServiceSorting = Core.Models.Service.Enums.ServiceSorting.Newest,
             };
+
+            // Act
             var services = serviceService.AllAsync(model).Result;
+
+            // Assert
             Assert.That(expected, Is.EqualTo(services.TotalServicesCount));
         }
 
         [Test]
         public void AllServiceListViewModelFilteredAsync_ShouldReturnCorrectServices()
         {
+            // Arrange
             int expectedNumber = 1;
+
+            // Act
             var services = serviceService.AllServiceListViewModelFilteredAsync(s => s.ServiceType == Common.Enumerations.ServiceType.Slider).Result;
+
+            // Assert
             Assert.That(expectedNumber, Is.EqualTo(services.Count()));
         }
 
         [Test]
         public void CreateServiceAsync_ShouldWorkProperly()
         {
+            // Arrange
             ServiceFormViewModel model = new ServiceFormViewModel()
             {
                 Title = "Service 3",
@@ -85,44 +109,130 @@ namespace EldenBoost.Tests.UnitTests
                 MaxAmount = 50
             };
 
+            // Act
             serviceService.CreateServiceAsync(model);
-
-            int expectedNumber = 2;
             var services = serviceService.AllServiceListViewModelFilteredAsync(s => s.ServiceType == Common.Enumerations.ServiceType.Slider).Result;
+
+            // Assert
+            int expectedNumber = 2;
             Assert.That(expectedNumber, Is.EqualTo(services.Count()));
         }
 
         [Test]
         public void DeactivateByIdAsync_ShouldDeactivateService()
         {
+            // Arrange
             Assert.IsTrue(Service2.IsActive);
 
+            // Act
             serviceService.DeactivateByIdAsync(Service2.Id);
 
+            // Assert
             Assert.IsFalse(Service2.IsActive);
+
+            // Cleanup
+            Service2.IsActive = true;
         }
 
         [Test]
         public void EditAsync_ShouldEditServiceCorrectly()
         {
+            // Arrange
             Assert.That(Service.Title, Is.EqualTo("Game Completion"));
 
             ServiceEditViewModel model = new ServiceEditViewModel()
             {
                 Id = 1,
                 Title = "Game Completion - Edited",
-                Description = "You will get a 100% game completion and all the items we aquire along the way!",
+                Description = "You will get a 100% game completion and all the items we acquire along the way!",
                 Price = 300.00M,
                 ImageURL = "/images/service/gameCompletion.jpg",
                 MaxAmount = 0,
                 ServiceType = ServiceType.Basic
             };
 
+            // Act
             serviceService.EditAsync(model);
 
+            // Assert
             Assert.That(Service.Title, Is.Not.EqualTo("Game Completion"));
             Assert.That(Service.Title, Is.EqualTo("Game Completion - Edited"));
+        }
 
+        [Test]
+        public void ExistsByIdAsync_ReturnsTrue_WhenIdIsCorrect()
+        {
+            // Arrange & Act
+            bool result = serviceService.ExistsByIdAsync(Service.Id).Result;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ExistsByIdAsync_ReturnsFalse_WhenIdIsIncorrect()
+        {
+            // Arrange & Act
+            bool result = serviceService.ExistsByIdAsync(5000).Result;
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void GetServiceDetailsViewModelByIdAsync_WorksCorrectly()
+        {
+            // Arrange
+            int expectedId = Service.Id;
+            string expectedTitle = Service.Title;
+            string expectedDescription = Service.Description;
+            string expectedImage = Service.ImageURL!;
+            decimal expectedPrice = Service.Price;
+            ServiceType expectedServiceType = Service.ServiceType;
+            int expectedMaxAmount = Service.MaxAmount;
+
+            // Act
+            var service = serviceService.GetServiceDetailsViewModelByIdAsync(Service.Id).Result;
+
+            // Assert
+            Assert.That(service!.Id, Is.EqualTo(expectedId));
+            Assert.That(service.Title, Is.EqualTo(expectedTitle));
+            Assert.That(service.Description, Is.EqualTo(expectedDescription));
+            Assert.That(service.ImageURL, Is.EqualTo(expectedImage));
+            Assert.That(service.Price, Is.EqualTo(expectedPrice));
+            Assert.That(service.ServiceType, Is.EqualTo(expectedServiceType));
+            Assert.That(service.MaxAmount, Is.EqualTo(expectedMaxAmount));
+        }
+
+        [Test]
+        public async Task GetPopularServicesAsync_ShouldReturnServices_InCorrectOrder()
+        {
+            // Arrange
+            Service2.PurchaseCount += 10;
+            data.Services.Update(Service2);
+            await data.SaveChangesAsync();
+
+            // Act
+            var services = await serviceService.GetPopularServicesAsync();
+
+            // Assert
+            Assert.That(services.First().Title, Is.EqualTo(Service2.Title), "Service2 should be first in the list.");
+        }
+
+        [Test]
+        public void GetServiceEditViewModelByIdAsync_ShouldReturnCorrectService()
+        {
+            // Arrange
+            int expectedId = Service2.Id;
+            string expectedTitle = Service2.Title;
+
+            // Act
+            var service = serviceService.GetServiceEditViewModelByIdAsync(Service2.Id).Result;
+
+            // Assert
+            Assert.That(service.Id, Is.EqualTo(expectedId));
+            Assert.That(service.Title, Is.EqualTo(expectedTitle));
         }
     }
 }
+
