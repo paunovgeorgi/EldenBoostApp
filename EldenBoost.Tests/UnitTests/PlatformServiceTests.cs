ced10031f05724322fae33142bcf78fbbc9ae50e
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-26 12:13:05 +0200

    PlatformServiceTests class created.

---
+++ EldenBoost.Tests/UnitTests/PlatformServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Services;
+using EldenBoost.Infrastructure.Data.Repository;
+
+namespace EldenBoost.Tests.UnitTests
+{
+    [TestFixture]
+    public class PlatformServiceTests : UnitTestsBase
+    {
+        private IRepository repository;
+        private IPlatformService platformService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            repository = new Repository(data);
+            platformService = new PlatformService(repository);
+        }
+    }
+}
