34656b38f305253c0b368288be4884b1451f21e7
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-26 11:53:34 +0200

    ReviewServiceTests created.

---
+++ EldenBoost.Tests/UnitTests/ReviewServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Services;
+using EldenBoost.Infrastructure.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Tests.UnitTests
+{
+    [TestFixture]
+    public class ReviewServiceTests : UnitTestsBase
+    {
+        private IRepository repository;
+        private IReviewService reviewService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            repository = new Repository(data);
+            reviewService = new ReviewService(repository);
+        }
+
+
+    }
+}
