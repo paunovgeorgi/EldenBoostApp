09d010068366d104206f320ea6a96884c097a49b
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-11-27 18:46:03 +0200

    ApplicationCountDataModel implemented.

---
+++ EldenBoost.Core/Models/Application/ApplicationCountDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Core.Models.Application
+{
+	public class ApplicationCountDataModel
+	{
+        public int BoosterPending { get; set; }
+        public int BoosterApproved { get; set; }
+        public int AuthorPending { get; set; }
+        public int AuthorApproved { get; set; }
+    }
+}
