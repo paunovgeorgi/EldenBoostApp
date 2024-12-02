278f43f2f769e464be6f187e6d6776ea2c03da27
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-02 10:41:05 +0200

    OrderServiceTests creasted, AllByBoosterIdAsync_ShouldReturnCorrectOrders test implemented.

---
+++ EldenBoost.Tests/UnitTests/OrderServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Services;
+using EldenBoost.Infrastructure.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenBoost.Tests.UnitTests
+{
+    [TestFixture]
+    public class OrderServiceTests : UnitTestsBase
+    {
+        private IRepository repository;
+        private IOrderService orderService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            repository = new Repository(data);
+            orderService = new OrderService(repository);
+        }
+
+        [Test]
+        public async Task AllByBoosterIdAsync_ShouldReturnCorrectOrders()
+        {
+            //Arrange
+            int boosterId = Booster.Id;
+
+            //Act
+            var orders = await orderService.AllByBoosterIdAsync(boosterId);
+
+            //Assert
+            Assert.IsEmpty(orders);
+
+            //Arrange
+            Order.BoosterId = boosterId;
+            await data.SaveChangesAsync();
+
+            //Act
+            var newOrders = await orderService.AllByBoosterIdAsync(boosterId);
+
+            //Assert
+            Assert.IsNotEmpty(newOrders);
+
+            //Cleanup
+            Order.BoosterId = null;
+            await data.SaveChangesAsync();
+        }
+    }
+}
