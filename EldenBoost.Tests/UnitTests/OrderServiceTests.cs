807d2a820cc9ad3dec946682d84aeaac972de9c2
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-02 16:22:23 +0200

    more tests implemented.

--- EldenBoost.Tests/UnitTests/OrderServiceTests.cs
+++ EldenBoost.Tests/UnitTests/OrderServiceTests.cs
@@ -48,5 +48,70 @@ namespace EldenBoost.Tests.UnitTests
             Order.BoosterId = null;
             await data.SaveChangesAsync();
         }
+
+        [Test]
+        public async Task AllByUserIdAsync_ShouldReturnCorrectOrders()
+        {
+            //Arrange
+            string userId = User.Id;
+            int expected = 1;
+
+            //Act
+            var orders = await orderService.AllByUserIdAsync(userId);
+
+            //Assert
+            Assert.IsNotEmpty(orders);
+            Assert.That(orders.Count(), Is.EqualTo(expected));
+        }
+
+
+        [Test]
+        public async Task AllOrdersAsync_ShouldReturnCorrectOrders()
+        {
+            //Arrange
+            string userId = User.Id;
+            int expected = 1;
+
+            //Act
+            var orders = await orderService.AllOrdersAsync();
+
+            //Assert
+            Assert.IsNotEmpty(orders);
+            Assert.That(orders.Count(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public async Task GetByIdAsync_ShouldReturnCorrectOrder()
+        {
+            //Arrange
+            int orderId = Order.Id;
+            string expectedStatus = "Pending";
+            string expectedClientId = User.Id;
+            string information = CartItem.Information!;
+
+            //Act
+            var order = await orderService.GetOrderByIdAsync(orderId);
+
+            //Assert
+            Assert.IsTrue(order != null);
+            Assert.IsTrue(!string.IsNullOrEmpty(information));
+            Assert.That(order!.Information, Is.EqualTo(information));
+            Assert.That(order.ClientId, Is.EqualTo(expectedClientId));
+            Assert.That(order.Status, Is.EqualTo(expectedStatus));
+        }
+
+        [Test]
+        public async Task GetByIdAsync_ShouldReturnNull_WithWrongId()
+        {
+            //Arrange
+            int orderId = 3500;
+
+            //Act
+            var order = await orderService.GetOrderByIdAsync(orderId);
+
+            //Assert
+            Assert.IsNull(order);
+        }
+
     }
 }
