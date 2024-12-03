d7bcdb380956c06e6f2765ed706f886ec82f7ef2
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-03 10:38:31 +0200

    AuthorServiceTests created, ExistsByUserIdAsync tests implemented.

---
+++ EldenBoost.Tests/UnitTests/AuthorServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using EldenBoost.Core.Contracts;
+using EldenBoost.Core.Services;
+using EldenBoost.Infrastructure.Data.Repository;
+
+namespace EldenBoost.Tests.UnitTests
+{
+    [TestFixture]
+    public class AuthorServiceTests : UnitTestsBase
+    {
+        private IRepository repository;
+        private IAuthorService authorService;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            repository = new Repository(data); 
+            authorService = new AuthorService(repository);
+        }
+
+        [Test]
+        public async Task ExistsByUserIdAsync_ShouldReturnTrue_WithValidUserId()
+        {
+            //Arrange
+            string userId = Author.UserId;
+
+            //Act
+            bool result = await authorService.ExistsByUserIdAsync(userId);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Test]
+        public async Task ExistsByUserIdAsync_ShouldReturnFalse_WithInvalidUserId()
+        {
+            //Arrange
+            string invalidId = Booster.UserId;
+
+            //Act
+            bool result = await authorService.ExistsByUserIdAsync(invalidId);
+
+            //Assert
+            Assert.False(result);
+        }
+
+    }
+}
