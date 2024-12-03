74bc84dc2e4f9ca89b78edeabd67821602cd5da2
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-03 11:03:31 +0200

    All AuthorServiceTests implemented.

--- EldenBoost.Tests/UnitTests/AuthorServiceTests.cs
+++ EldenBoost.Tests/UnitTests/AuthorServiceTests.cs
@@ -43,5 +43,114 @@ namespace EldenBoost.Tests.UnitTests
             Assert.False(result);
         }
 
+        [Test]
+        public async Task GetAuthorByUserIdAsync_ShouldReturnCorrectAuthor()
+        {
+            //Arrange
+            string validUserId = Author.UserId;
+            string expectedCountry = Author.Country;
+            int expectedAuthorId = Author.Id;
+
+            //Act
+            var author = await authorService.GetAuthorByUserIdAsync(validUserId);
+
+            //Assert
+            Assert.NotNull(author);
+            Assert.That(author.Country, Is.EqualTo(expectedCountry));
+            Assert.That(author.Id, Is.EqualTo(expectedAuthorId));
+        }
+
+        [Test]
+        public async Task GetAuthorByUserIdAsync_ShouldReturnNull_WithInvalidUserId()
+        {
+            //Arrange
+            string invalidUserId = User.Id;
+
+            //Act
+            var author = await authorService.GetAuthorByUserIdAsync(invalidUserId);
+
+            //Assert
+            Assert.IsNull(author);
+        }
+
+        [Test]
+        public async Task HasArticleAsync_ShouldReturnTrue_WithValidUserAndArticleIds()
+        {
+            //Arrange
+            string authorUserId = Author.UserId;
+            int articleId = Article.Id;
+
+            //Act
+            bool result = await authorService.HasArticleAsync(authorUserId, articleId);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task HasArticleAsync_ShouldReturnFalse_WithInvalidUserIdAndValidArticleId()
+        {
+            //Arrange
+            string invalidUserId = User.Id;
+            int articleId = Article.Id;
+
+            //Act
+            bool result = await authorService.HasArticleAsync(invalidUserId, articleId);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task HasArticleAsync_ShouldReturnFalse_WithValidUserId_WithNoArticles()
+        {
+            //Arrange
+            string authorUserId = Author.UserId;
+            int articleId = Article.Id;
+            Article.AuthorId = 200;
+            await data.SaveChangesAsync();
+
+            //Act
+            bool result = await authorService.HasArticleAsync(authorUserId, articleId);
+
+            //Assert
+            Assert.IsFalse(result);
+
+            //Cleanup
+            Article.AuthorId = Author.Id;
+            await data.SaveChangesAsync();
+        }
+
+        [Test]
+        public async Task IsActiveAsync_ShouldReturnTrue_WhenAuthorIseDemoted_IsFalse()
+        {
+            //Arrange
+            string authorUserId = Author.UserId;
+
+            //Act
+            bool result = await authorService.IsActiveAsync(authorUserId);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task IsActiveAsync_ShouldReturnFalse_WhenAuthorIsDemoted_IsTrue()
+        {
+            //Arrange
+            string authorUserId = Author.UserId;
+            Author.IsDemoted = true;
+            await data.SaveChangesAsync();
+
+            //Act
+            bool result = await authorService.IsActiveAsync(authorUserId);
+
+            //Assert
+            Assert.IsFalse(result);
+
+            //Cleanup
+            Author.IsDemoted = false;
+            await data.SaveChangesAsync();
+        }
     }
 }
