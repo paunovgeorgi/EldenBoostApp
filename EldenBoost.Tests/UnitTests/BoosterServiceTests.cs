d72c75843168e9c54c2312758a41653ac9c1e89a
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-03 20:31:07 +0200

    BoosterServiceTests completed.

--- EldenBoost.Tests/UnitTests/BoosterServiceTests.cs
+++ EldenBoost.Tests/UnitTests/BoosterServiceTests.cs
@@ -74,5 +74,152 @@ namespace EldenBoost.Tests.UnitTests
             //Assert
             Assert.IsFalse(result);
         }
+
+        [Test]
+        public async Task GetBoosterByBoosterIdAsync_ShouldReturnCorrectBooster_WithValidBoosterId()
+        {
+            //Arrange
+            int boosterId = Booster.Id;
+            string expectedCountry = Booster.Country;
+            double expectedRating = Booster.Rating;
+
+            //Act
+            var booster = await boosterService.GetBoosterByBoosterIdAsync(boosterId);
+
+            //Assert
+            Assert.IsNotNull(booster);
+            Assert.That(booster.Country, Is.EqualTo(expectedCountry));
+            Assert.That(booster.Rating, Is.EqualTo(expectedRating));
+        }
+
+        [Test]
+        public async Task GetBoosterByBoosterIdAsync_ShouldReturnNull_WithIncorrectBoosterId()
+        {
+            //Arrange
+            int incorrectId = 3000;
+
+            //Act
+            var booster = await boosterService.GetBoosterByBoosterIdAsync(incorrectId);
+
+            //Assert
+            Assert.IsNull(booster);
+        }
+
+        [Test]
+        public async Task GetBoosterByUserIdAsync_ShouldReturnCorrectBooster_WithCorrectUserId()
+        {
+            //Arrange
+            string userId = Booster.UserId;
+            string expectedCountry = Booster.Country;
+            double expectedRating = Booster.Rating;
+
+            //Act
+            var booster = await boosterService.GetBoosterByUserIdAsync(userId);
+
+            //Assert
+            Assert.IsNotNull(booster);
+            Assert.That(booster.Country, Is.EqualTo(expectedCountry));
+            Assert.That(booster.Rating, Is.EqualTo(expectedRating));
+        }
+
+        [Test]
+        public async Task GetBoosterByUserIdAsync_ShouldReturnNull_WithIncorrectUserId()
+        {
+            //Arrange
+            string incorrectUserId = Author.UserId;
+
+            //Act
+            var booster = await boosterService.GetBoosterByUserIdAsync(incorrectUserId);
+
+            //Assert
+            Assert.IsNull(booster);
+        }
+
+        [Test]
+        public async Task GetBoosterIdAsync_ShouldReturnCorrectId()
+        {
+            //Arrange
+            string userId = Booster.UserId;
+            int expectedId = Booster.Id;
+
+            //Act
+            int result = await boosterService.GetBoosterIdAsync(userId);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedId));
+        }
+
+        [Test]
+        public async Task GetBoosterIdAsync_ShouldReturnZero_WithIncorrectUserId()
+        {
+            //Arrange
+            string incorrectId = Author.UserId;
+
+            //Act
+            int result = await boosterService.GetBoosterIdAsync(incorrectId);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task IsActiveAsync_ShouldReturnTrue_WhenBoosterIseDemoted_IsFalse()
+        {
+            //Arrange
+            string boosterUserId = Booster.UserId;
+
+            //Act
+            bool result = await boosterService.IsActiveAsync(boosterUserId);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task IsActiveAsync_ShouldReturnFalse_WhenBoosterIsDemoted_IsTrue()
+        {
+            //Arrange
+            string boosterUserId = Booster.UserId;
+            Booster.IsDemoted = true;
+            await data.SaveChangesAsync();
+
+            //Act
+            bool result = await boosterService.IsActiveAsync(boosterUserId);
+
+            //Assert
+            Assert.IsFalse(result);
+
+            //Cleanup
+            Booster.IsDemoted = false;
+            await data.SaveChangesAsync();
+        }
+
+        [Test]
+        public async Task RateAsync_ShouldWorkCorrectly()
+        {
+            //Arrange
+            int currentRating = 0;
+            int currentRatingCount = 0;
+            int addedRating = 5;
+            int expectedRating = 5;
+            int expectedRatingCount = 1;
+            int boosterId = Booster.Id;
+
+            //Assert current conditions
+            Assert.That(Booster.Rating, Is.EqualTo(currentRating));
+            Assert.That(Booster.RatingCount, Is.EqualTo(currentRatingCount));
+
+            //Act
+            await boosterService.RateAsync(boosterId, addedRating);
+
+            //Assert
+            Assert.That(Booster.Rating, Is.EqualTo(expectedRating));
+            Assert.That(Booster.RatingCount, Is.EqualTo(expectedRatingCount));
+
+            //Cleanup
+            Booster.Rating = 0;
+            Booster.RatingCount = 0;
+            await data.SaveChangesAsync();
+        }
     }
 }
