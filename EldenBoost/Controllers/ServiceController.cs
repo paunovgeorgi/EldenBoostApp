91d093a4a4cef06e4da92cd28fb26b53f9cc40b2
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-10 20:41:06 +0200

    ServiceController updated (comments, logging, additional checks added) || Buy action removed.

--- EldenBoost/Controllers/ServiceController.cs
+++ EldenBoost/Controllers/ServiceController.cs
@@ -15,47 +15,64 @@ namespace EldenBoost.Controllers
         private readonly IOrderService orderService;
         private readonly IBoosterService boosterService;
         private readonly IPlatformService platformService;
+        private readonly ILogger<ServiceController> logger;
 
         public ServiceController(
             IServiceService _serviceService,
             IOrderService _orderService,
             IBoosterService _boosterService,
-            IPlatformService _platformService)
+            IPlatformService _platformService,
+            ILogger<ServiceController> _logger)
         {
             serviceService = _serviceService;
             orderService = _orderService;
             boosterService = _boosterService;
             platformService = _platformService;
+            logger = _logger;
         }
 
         [HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> All([FromQuery] AllServicesQueryModel model)
         {
-			//Gets filtered and paged collection of ServiceCardViewModel for all services.
-			AllServicesFilteredAndPagedModel serviceModel = await serviceService.AllAsync(model);
+            try
+            {
+                //Fetches filtered and paged collection of ServiceCardViewModel for all services
+                AllServicesFilteredAndPagedModel serviceModel = await serviceService.AllAsync(model);
 
-            model.Services = serviceModel.Services;
-            model.TotalServices = serviceModel.TotalServicesCount;
+                model.Services = serviceModel.Services;
+                model.TotalServices = serviceModel.TotalServicesCount;
 
-            return View(model);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while fetching all services.");
+                return RedirectToAction("Index", "Home");
+            }
         }
 
         [HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> Popular()
         {
-			//Gets services in ServiceCardViewModel ordered by purchase count.
-			var model = await serviceService.GetPopularServicesAsync();
-
-            return View(model);
+            try
+            {
+                // Gets the most popular services based on purchase count.
+                var model = await serviceService.GetPopularServicesAsync();
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while fetching popular services.");
+                return RedirectToAction("Index", "Home");
+            }
         }
 
         [HttpGet]
         [AllowAnonymous]
         public async Task<IActionResult> Details(int id, string information)
-        {
-            
+        {           
             var model = await serviceService.GetServiceDetailsViewModelByIdAsync(id);
 
             if (model == null)
@@ -75,35 +92,5 @@ namespace EldenBoost.Controllers
 
             return View(model);
         }
-
-		[HttpPost]
-		public async Task<IActionResult> Buy(int serviceId, int platformId, decimal? updatedPrice, bool hasStream, bool isExpress, int? optionId, int sliderValue)
-		{
-			//Check if service exists.
-			if (await serviceService.ExistsByIdAsync(serviceId) == false)
-			{
-				return BadRequest("Service doesn't exist!");
-			}
-
-			//Check if platform exists.
-			if (await platformService.PlatformExistsByIdAsync(platformId) == false)
-			{
-				return BadRequest("Platform doesn't exist!");
-			}
-            
-            //Check if user is booster, if so - redirect.
-			if (await boosterService.BoosterExistsByUserIdAsync(User.Id()))
-			{
-				TempData[ErrorMessage] = "You are a booster mate, can't purchase services.";
-				return RedirectToAction("All", "Service");
-			}
-
-			TempData[SuccessMessage] = "Your purchase was successful!";
-
-            //Create order.
-			await orderService.CreateOrderAsync(serviceId, User.Id(), platformId, updatedPrice, hasStream, isExpress, optionId, sliderValue);
-            //return RedirectToAction("MyProfile", "Client");
-            return RedirectToAction(nameof(All));
-		}
-	}
+    }
 }
