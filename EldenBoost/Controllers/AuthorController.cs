1b0314ea3af284e62a98baa7c52fb72ec51db889
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-10 17:04:00 +0200

    AuthorController updated (comments, logger, additional checks added)

--- EldenBoost/Controllers/AuthorController.cs
+++ EldenBoost/Controllers/AuthorController.cs
@@ -9,24 +9,37 @@ namespace EldenBoost.Controllers
     {
         private readonly IAuthorService authorService;
         private readonly IArticleService articleService;
+        private readonly ILogger<AuthorController> logger;
 
-        public AuthorController(IAuthorService _authorService, IArticleService _articleService)
+        public AuthorController(IAuthorService _authorService, IArticleService _articleService, ILogger<AuthorController> _logger)
         {
             authorService = _authorService;
             articleService = _articleService;
+            logger = _logger;
         }
         [HttpGet]
+        // Fetches the author's profile and their articles
         public async Task<IActionResult> MyProfile()
         {
-            Author? author = await authorService.GetAuthorByUserIdAsync(User.Id());
-            if (author == null)
+            try
             {
-                return Unauthorized("You don't have access to this page!");
+                // Get the author details based on the logged-in user's ID
+                Author? author = await authorService.GetAuthorByUserIdAsync(User.Id());
+                if (author == null)
+                {
+                    logger.LogWarning($"User with ID {User.Id()} tried to access the profile but is not an author.");
+                    return Unauthorized("You don't have access to this page!");
+                }
+
+                // Fetch articles written by the author
+                var articles = await articleService.AllByAuthorIdAsync(author.Id);
+                return View(articles);
             }
-
-            var articles = await articleService.AllByAuthorIdAsync(author.Id);
-
-            return View(articles);
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while fetching author profile and articles.");
+                return StatusCode(500, "Internal server error");
+            }
         }
     }
 }
