d979d30a1856029f47e2fef0641a4e3d3ddde52e
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-12-11 12:19:35 +0200

    Admin Area - UserController - updated (comments, logger, additional checks added)

--- EldenBoost/Areas/Admin/Controllers/UserController.cs
+++ EldenBoost/Areas/Admin/Controllers/UserController.cs
@@ -7,33 +7,62 @@ namespace EldenBoost.Areas.Admin.Controllers
     public class UserController : BaseAdminController
     {
         private readonly IUserService userService;
-        public UserController(IUserService _userService)
+        private readonly ILogger<UserController> logger;
+        public UserController(IUserService _userService, ILogger<UserController> _logger)
         {
             userService = _userService;
+            logger = _logger;
         }
 
+        // Retrieve all users
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var users = await userService.AllAsync();
-            return View(users);
+            try
+            {
+                var users = await userService.AllAsync();
+                return View(users);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while retrieving users.");
+                TempData[ErrorMessage] = "Failed to load users.";
+                return RedirectToAction("Index", "Home");
+            }
         }
 
+        // Demote user from Booster or Author position
         [HttpPost]
         public async Task<IActionResult> Demote(string userId)
         {
-            await userService.DemoteAsync(userId);
+            try
+            {
+                await userService.DemoteAsync(userId);
+                TempData[SuccessMessage] = "User successfully demoted!";
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while demoting user with ID {UserId}.", userId);
+                TempData[ErrorMessage] = "Failed to demote the user.";
+            }
 
-            TempData[SuccessMessage] = "User successfully Demoted!";
             return RedirectToAction(nameof(All));
         }
 
+        // Reinstate user to Booster or Author position
         [HttpPost]
         public async Task<IActionResult> Reinstate(string userId)
         {
-            await userService.ReinstateAsync(userId);
-
-            TempData[SuccessMessage] = "User successfully Reinstated!";
+            try
+            {
+                await userService.ReinstateAsync(userId);
+                TempData[SuccessMessage] = "User successfully reinstated!";
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while reinstating user with ID {UserId}.", userId);
+                TempData[ErrorMessage] = "Failed to reinstate the user.";
+            }
             return RedirectToAction(nameof(All));
         }
     }
